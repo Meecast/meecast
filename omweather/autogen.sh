103f8fa7210d117cd3189a7006340f41ec2919e5
pavel <pavel>
2007-03-02 10:31:02 +0000

    changes for i18n
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@257 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/autogen.sh
+++ omweather/autogen.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-#set -x
-#aclocal-1.7
-#autoconf
-#libtoolize
-#automake-1.7 --add-missing --foreign
-
 set -x
 glib-gettextize --copy --force
 libtoolize --automake --copy --force
-intltoolize --automake --copy --force
 aclocal
+intltoolize --automake
 autoconf --force
 autoheader --force
 automake --add-missing --copy --force-missing --foreign

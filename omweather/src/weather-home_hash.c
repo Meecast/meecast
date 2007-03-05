1f60844b6eb6523d515610c2e27393be8296f216
pavel <pavel>
2007-03-05 18:46:59 +0000

    new files for i18n
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@284 5aa3d57c-301e-0410-a7f2-8fec96c11689

---
+++ omweather/src/weather-home_hash.c
@@ -0,0 +1,68 @@
+#include "weather-home_hash.h"
+#include <glib.h>
+
+static GHashTable *hash;
+
+void hash_table_create(){
+    hash = g_hash_table_new(g_str_hash, g_str_equal);
+    g_hash_table_insert(hash, "AM Rain / Snow Showers", _("AM Rain / Snow Showers"));
+    g_hash_table_insert(hash, "AM Showers", _("AM Showers"));
+    g_hash_table_insert(hash, "AM Snow Showers", _("AM Snow Showers"));
+    g_hash_table_insert(hash, "CALM", _("CALM"));
+    g_hash_table_insert(hash, "Clear", _("Clear"));
+    g_hash_table_insert(hash, "Cloudy", _("Cloudy"));
+    g_hash_table_insert(hash, "E", _("E"));
+    g_hash_table_insert(hash, "ENE", _("ENE"));
+    g_hash_table_insert(hash, "ESE", _("ESE"));
+    g_hash_table_insert(hash, "Fair", _("Fair"));
+    g_hash_table_insert(hash, "Few Showers", _("Few Showers"));
+    g_hash_table_insert(hash, "Few Snow Showers", _("Few Snow Showers"));
+    g_hash_table_insert(hash, "Full", _("Full"));
+    g_hash_table_insert(hash, "High", _("High"));
+    g_hash_table_insert(hash, "Isolated T-Storms", _("Isolated T-Storms"));
+    g_hash_table_insert(hash, "Light Rain", _("Light Rain"));
+    g_hash_table_insert(hash, "Low", _("Low"));
+    g_hash_table_insert(hash, "Mostly Clear", _("Mostly Clear"));
+    g_hash_table_insert(hash, "Mostly Cloudy", _("Mostly Cloudy"));
+    g_hash_table_insert(hash, "Mostly Sunny", _("Mostly Sunny"));
+    g_hash_table_insert(hash, "N/A", _("N/A"));
+    g_hash_table_insert(hash, "NE", _("NE"));
+    g_hash_table_insert(hash, "N", _("N"));
+    g_hash_table_insert(hash, "NNE", _("NNE"));
+    g_hash_table_insert(hash, "NNW", _("NNW"));
+    g_hash_table_insert(hash, "NW", _("NW"));
+    g_hash_table_insert(hash, "Partly Cloudy", _("Partly Cloudy"));
+    g_hash_table_insert(hash, "PM Snow Showers", _("PM Snow Showers"));
+    g_hash_table_insert(hash, "Rain / Snow", _("Rain / Snow"));
+    g_hash_table_insert(hash, "Rain / Snow Showers Late", _("Rain / Snow Showers Late"));
+    g_hash_table_insert(hash, "Rain / Snow Showers", _("Rain / Snow Showers"));
+    g_hash_table_insert(hash, "Scattered Showers", _("Scattered Showers"));
+    g_hash_table_insert(hash, "Scattered Snow Showers", _("Scattered Snow Showers"));
+    g_hash_table_insert(hash, "Scattered T-Storms", _("Scattered T-Storms"));
+    g_hash_table_insert(hash, "SE", _("SE"));
+    g_hash_table_insert(hash, "Showers Late", _("Showers Late"));
+    g_hash_table_insert(hash, "Showers", _("Showers"));
+    g_hash_table_insert(hash, "Snow Showers Late", _("Snow Showers Late"));
+    g_hash_table_insert(hash, "Snow Shower", _("Snow Shower"));
+    g_hash_table_insert(hash, "Snow to Rain", _("Snow to Rain"));
+    g_hash_table_insert(hash, "S", _("S"));
+    g_hash_table_insert(hash, "SSE", _("SSE"));
+    g_hash_table_insert(hash, "SSW", _("SSW"));
+    g_hash_table_insert(hash, "Sunny", _("Sunny"));
+    g_hash_table_insert(hash, "SW", _("SW"));
+    g_hash_table_insert(hash, "Waning Gibbous", _("Waning Gibbous"));
+    g_hash_table_insert(hash, "WNW", _("WNW"));
+    g_hash_table_insert(hash, "WSW", _("WSW"));
+    g_hash_table_insert(hash, "W", _("W"));
+}
+
+gpointer hash_table_find(gpointer key){
+    gpointer	orig_key,
+		value;
+    if(g_hash_table_lookup_extended(hash,
+				    key,
+				    &orig_key,
+				    &value))
+	return value;
+    return key;
+}
---
+++ omweather/src/weather-home_hash.h
@@ -0,0 +1,9 @@
+#ifndef _weather_home_hash_h
+#define _weather_home_hash_h 1
+
+#include "weather-home_common.h"
+
+void hash_table_create();
+gpointer hash_table_find(gpointer key);
+
+#endif

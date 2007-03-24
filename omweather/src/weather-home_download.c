20be44f17717627073acee69502ea74c872f195b
pavel <pavel>
2007-03-24 20:31:15 +0000

    align code
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@412 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-home_download.c
+++ omweather/src/weather-home_download.c
@@ -67,11 +67,9 @@ gboolean get_connected(void){
 
 
 /* Init easy curl */
-CURL 
-*weather_curl_init(void)
-{
-CURL *curl_handle;
-    curl_handle  = curl_easy_init(); 
+CURL* weather_curl_init(void){
+    CURL *curl_handle;
+    curl_handle = curl_easy_init(); 
     curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1); 
     curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1); 
     curl_easy_setopt(curl_handle, CURLOPT_FAILONERROR, 1); 
@@ -82,111 +80,79 @@ CURL *curl_handle;
     curl_easy_setopt(curl_handle, CURLOPT_CONNECTTIMEOUT, 10); 
     config_update_proxy();
     /* Set Proxy option */
-    if(app->iap_http_proxy_host) 
-    { 
+    if(app->iap_http_proxy_host){ 
         curl_easy_setopt(curl_handle, CURLOPT_PROXY, app->iap_http_proxy_host); 
         if(app->iap_http_proxy_port) 
             curl_easy_setopt(curl_handle, CURLOPT_PROXYPORT, app->iap_http_proxy_port); 
     } 
-
     return curl_handle;    
 }
 
-int data_read(void *buffer, size_t size, size_t nmemb, void *stream)
-{
-  int result;
-  struct HtmlFile *out=(struct HtmlFile *)stream;
+int data_read(void *buffer, size_t size, size_t nmemb, void *stream){
+    int result;
+    struct HtmlFile *out=(struct HtmlFile *)stream;
 
-  /* For debug    
-  fprintf(stderr,"%s()\n", __PRETTY_FUNCTION__);
-  */
-  if(out && !out->stream) {
-  /* open file for writing */
-  out->stream=fopen(out->filename, "wb");
-  if(!out->stream)
-  {
-      return -1; /* failure, can't open file to write */      
-  }      
-  }
-  result = fwrite(buffer, size, nmemb, out->stream);
-  return result;
-
+    if(out && !out->stream){
+    /* open file for writing */
+	out->stream = fopen(out->filename, "wb");
+	if(!out->stream)
+	    return -1; /* failure, can't open file to write */      
+    }
+    result = fwrite(buffer, size, nmemb, out->stream);
+    return result;
 }			  
 
 /* Form URL and filename for  write xml file. 
    Returns TRUE if the station is taken from the list
    Else return FLASE. This the end list
 */
-gboolean
-form_url_and_filename()
-{
-    /* For debug  
-    fprintf(stderr,"%s()\n", __PRETTY_FUNCTION__);    
-     */
-    if (tmplist != NULL)
-    {
+gboolean form_url_and_filename(){
+    if(tmplist != NULL){
         ws = tmplist->data;
-        if(ws->id_station != NULL)
-	    {
-	        if (url){
-		 g_string_free(url, TRUE);    
-		 url = NULL;
-		} 
-	        if (full_filename_new_xml){
-	         g_string_free(full_filename_new_xml, TRUE);    
-		 full_filename_new_xml = NULL;
-		} 
-		url = g_string_new(NULL);        
-    		g_string_append_printf(url,"http://xoap.weather.com/weather/local/%s?cc=*&prod=xoap&par=1004517364&key=a29796f587f206b2&unit=m&dayf=%d",
-			    ws->id_station, DAY_DOWNLOAD);
-		full_filename_new_xml = g_string_new(NULL);        
-		g_string_append_printf(full_filename_new_xml,"%s/%s.xml.new",
-			app->_weather_dir_name, ws->id_station);
-		/*For debug
-		fprintf (stderr,"Begin URL: %s\n",url->str);
-		*/
-		tmplist = g_slist_next(tmplist);
-
-		/* Forming structure for download data of weather */
-		html_file.filename = full_filename_new_xml->str;
-                html_file.stream = NULL;
-		return TRUE;
-	    }
-	    else
-	     return FALSE;	
+        if(ws->id_station != NULL){
+	    if(url){
+		g_string_free(url, TRUE);    
+		url = NULL;
+	    } 
+	    if(full_filename_new_xml){
+	        g_string_free(full_filename_new_xml, TRUE);    
+		full_filename_new_xml = NULL;
+	    } 
+	    url = g_string_new(NULL);        
+    	    g_string_append_printf(url,"http://xoap.weather.com/weather/local/%s?cc=*&prod=xoap&par=1004517364&key=a29796f587f206b2&unit=m&dayf=%d",
+				    ws->id_station, DAY_DOWNLOAD);
+	    full_filename_new_xml = g_string_new(NULL);        
+	    g_string_append_printf(full_filename_new_xml,"%s/%s.xml.new",
+				    app->_weather_dir_name, ws->id_station);
+	    tmplist = g_slist_next(tmplist);
+	    /* Forming structure for download data of weather */
+	    html_file.filename = full_filename_new_xml->str;
+    	    html_file.stream = NULL;
+	    return TRUE;
 	}
 	else
-	{
-	    return FALSE;
-	}
-
+	    return FALSE;	
+    }
+    else
+	return FALSE;
 }
 
 /* Download html/xml file. Call every 100 ms after begin download*/
-gboolean
-download_html(void)
-{
+gboolean download_html(void){
 
- CURLMsg *msg;
-    /* For debug  
-    fprintf(stderr,"%s()\n", __PRETTY_FUNCTION__);    
-     */
+    CURLMsg *msg;
 
-    if(app->popup_window)
-    {
+    if(app->popup_window){
 	gtk_widget_destroy(app->popup_window);   
         app->popup_window=NULL;
     }
     /* For emulator only for update forecast*/
     second_attempt = TRUE;    
-    
-    if ( app->show_update_window && (!second_attempt))
-    {
+    if( app->show_update_window && (!second_attempt) ){
         get_connected();        
         return FALSE;
     }
     second_attempt = FALSE;
-    
     /* The second stage */
     /* call curl_multi_perform for read weather data from Inet */
     if(curl_multi && CURLM_CALL_MULTI_PERFORM
@@ -194,27 +160,22 @@ download_html(void)
         return TRUE; /* return to UI */
 
     /* The first stage */
-    if (!curl_handle)
-    {
-	if  (app->show_update_window)
-	{
+    if(!curl_handle){
+	if(app->show_update_window)
     	    create_window_update(); /* Window with update information */
-	}    
         /* Initialize list */
         tmplist = stations_view_list;
-	if (!form_url_and_filename())
-	{
-	    if (url){
-		 g_string_free(url, TRUE);    
-		 url = NULL;
+	if(!form_url_and_filename()){
+	    if(url){
+		g_string_free(url, TRUE);    
+		url = NULL;
 	    }	 
-	    if (full_filename_new_xml){
-	         g_string_free(full_filename_new_xml, TRUE);    
-		 full_filename_new_xml = NULL;
+	    if(full_filename_new_xml){
+	        g_string_free(full_filename_new_xml, TRUE);    
+		full_filename_new_xml = NULL;
 	    }	 
 	    return FALSE; /* The strange error */		
 	}    
-
 	/* Init easy_curl */
 	curl_handle = weather_curl_init();
 	curl_easy_setopt(curl_handle, CURLOPT_URL, url->str);
@@ -230,74 +191,58 @@ download_html(void)
         curl_multi_add_handle(curl_multi, curl_handle);	
         return TRUE; /* return to UI */
     }
-    else
-    {
+    else{
         /* The third stage */
-	while(curl_multi && (msg = curl_multi_info_read(curl_multi, &num_msgs)))
-	{
-	 if(msg->msg == CURLMSG_DONE) 
-         {	  
-	  if (msg->data.result != CURLE_OK) /* Not success of the download */
-	  {
-	   if (app->show_update_window)
-	   {
-		hildon_banner_show_information(app->main_window, 
-			    NULL, _("Did not download weather"));
-	   }		    
-	  }
-	  else
-	  {
-	   /* Clean */
-	   curl_multi_remove_handle(curl_multi,msg->easy_handle); /* Delete curl_handle from curl_multi */
-	   if (html_file.stream)
-        	fclose (html_file.stream);
-		
-	   if (!form_url_and_filename()) /* Success - all is downloaded */
-	   {
-	    if  (app->show_update_window)
-	    {
-		hildon_banner_show_information(app->main_window,
-				NULL, _("Weather updated"));	    
-	    }			
-             weather_frame_update(FALSE);
-	   }
-	   else
-	   {
-		/* set options for the curl easy handle */
-		curl_easy_setopt(curl_handle, CURLOPT_URL, url->str);
-        	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &html_file);		
-        	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, data_read);	
-    		/* add the easy handle to a multi session */
-    		curl_multi_add_handle(curl_multi, curl_handle);	
-		return TRUE;/* Download next station */
-	   }		    
-	  }
-	   
-	  if(update_window){
-		gtk_widget_destroy(update_window);
-		update_window = NULL;
-	  }
+	while(curl_multi && (msg = curl_multi_info_read(curl_multi, &num_msgs))){
+	    if(msg->msg == CURLMSG_DONE){	  
+		if(msg->data.result != CURLE_OK){ /* Not success of the download */
+		    if(app->show_update_window)
+			hildon_banner_show_information(app->main_window, 
+							NULL,
+							_("Did not download weather"));
+		}
+		else{ /* Clean */
+		    curl_multi_remove_handle(curl_multi,msg->easy_handle); /* Delete curl_handle from curl_multi */
+		    if(html_file.stream)
+        		fclose(html_file.stream);
+		    if(!form_url_and_filename()){ /* Success - all is downloaded */
+			if(app->show_update_window)
+			    hildon_banner_show_information(app->main_window,
+							    NULL,
+							    _("Weather updated"));	    
+        		weather_frame_update(FALSE);
+		    }
+		    else{
+			/* set options for the curl easy handle */
+			curl_easy_setopt(curl_handle, CURLOPT_URL, url->str);
+        		curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &html_file);		
+        		curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, data_read);	
+    			/* add the easy handle to a multi session */
+    			curl_multi_add_handle(curl_multi, curl_handle);	
+			return TRUE;/* Download next station */
+		    }		    
+		}
+		if(update_window){
+		    gtk_widget_destroy(update_window);
+		    update_window = NULL;
+		}
 	  /* Clean all */    
-          curl_multi_remove_handle(curl_multi,msg->easy_handle);
-	  curl_multi_cleanup(curl_multi);
-	  curl_multi = NULL;
-	  curl_handle = NULL;
-	  if (url){
-	    g_string_free(url, TRUE);    
-	     url = NULL;
-	  }	 
-	  if (full_filename_new_xml)
-	  {
-	     g_string_free(full_filename_new_xml, TRUE);    	  
-	     full_filename_new_xml = NULL;
-	  }     
-		 
-
-		 
-	  return FALSE; /* This is the end */
-	 }
+    		curl_multi_remove_handle(curl_multi,msg->easy_handle);
+		curl_multi_cleanup(curl_multi);
+		curl_multi = NULL;
+		curl_handle = NULL;
+		if(url){
+		    g_string_free(url, TRUE);    
+		    url = NULL;
+		}	 
+		if(full_filename_new_xml){
+		    g_string_free(full_filename_new_xml, TRUE);    	  
+		    full_filename_new_xml = NULL;
+		}     
+		return FALSE; /* This is the end */
+	    }
 	}
-       return TRUE;
+	return TRUE;
     }
-  return FALSE;
+    return FALSE;
 }

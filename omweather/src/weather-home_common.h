94437f86e07772e09355d998de8024fe1f349f0f
pavel <pavel>
2007-07-03 12:42:29 +0000

    added field to store zone info
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@759 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-home_common.h
+++ omweather/src/weather-home_common.h
@@ -89,7 +89,8 @@ typedef struct{
     part_of_day	day;		/* Or current weather */
     part_of_day	night;
     gchar	date[40];	/* Date */     
-    time_t	date_time;	/* Date of the year or current time*/  
+    time_t	date_time;	/* Date of the year or current time*/
+    time_t	zone;		/* time zone */
     gchar	dayshname[60];	/* Short name of day */
     gchar	dayfuname[60];	/* Full name of day */
     gchar	hi_temp[20];	/* High temperature of day or real current temperature for current day */

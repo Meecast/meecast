10aaeb3c15a74fd038f5bd3031dc3ef7d4b3852f
pavel <pavel>
2007-07-28 09:14:11 +0000

    add missing function prototype
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@914 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-window_popup.h
+++ omweather/src/weather-window_popup.h
@@ -43,13 +43,13 @@ GtkWidget* create_temperature_range_widget(int i);
 GtkWidget* create_24_hours_widget(int i, time_t current_time);
 GtkWidget* create_sun_time_widget(int i);
 void weather_window_popup_show(GtkWidget *widget,GdkEvent *event,gpointer user_data);
-void weather_window_preference (GtkWidget *widget,GdkEvent *event,gpointer user_data);
-int c2f(int temp);
-void set_font_size(GtkWidget *widget, char font_size);
+void weather_window_settings(GtkWidget *widget,GdkEvent *event,gpointer user_data);
 int convert_wind_units(int to, int value);
 /* extern functions */
 extern gpointer hash_table_find(gpointer key);
 extern void weather_initialize_dbus(void);
 extern void swap_temperature(int *hi, int *low);
+extern int c2f(int temp);
+extern void set_font_size(GtkWidget *widget, char font_size);
 /*******************************************************************************/
 #endif

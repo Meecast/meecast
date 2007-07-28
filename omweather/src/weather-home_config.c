89005e1f91dfe28f2f2e426fbeb885ae12e126a3
pavel <pavel>
2007-07-28 09:10:43 +0000

    correct some code, delete unused functions
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@910 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-home_config.c
+++ omweather/src/weather-home_config.c
@@ -32,9 +32,7 @@
 #include <fcntl.h>
 #include <errno.h>
 /*******************************************************************************/
-/*
- * Change the weather cache directory and update dependent variables.
- */
+/* Change the weather cache directory and update dependent variables. */
 gboolean config_set_weather_dir_name(gchar *new_weather_dir_name){
     GnomeVFSURI *weather_dir_uri;
     gboolean retval = FALSE;
@@ -100,106 +98,30 @@ void fill_station_from_clock_plugin_data(void){
 	while(!feof(clock_file)){
 	    memset(out_buffer, 0, sizeof(out_buffer)); /* Clear buffer */
 	    fgets(out_buffer, sizeof(out_buffer), clock_file); /* Read Next Line */
-	    tmp = strchr(out_buffer,'|'); /* Finding of a separator */
-	    if ((strncmp(out_buffer,home_city,tmp-out_buffer)==0) || 
-	        (strncmp(out_buffer,remote_city,tmp-out_buffer)==0))
-	    {
+	    tmp = strchr(out_buffer, '|'); /* Finding of a separator */
+	    if ((strncmp(out_buffer, home_city,tmp - out_buffer) == 0) || 
+	        (strncmp(out_buffer, remote_city,tmp - out_buffer) == 0)){
 		/* Prepare struct */
     		ws = g_new0(struct weather_station, 1);
-		tmp[strlen(tmp)-1] = 0;
-		ws->id_station = g_strdup(tmp+1);
+		tmp[ strlen(tmp) - 1 ] = 0;
+		ws->id_station = g_strdup(tmp + 1);
 		tmp[0] = 0;
 		ws->name_station = g_strdup(out_buffer);
 		/* Add station to stations list */
 		stations_view_list = g_slist_append(stations_view_list, ws); 
 		/* A current station */
-		if (strncmp(out_buffer,home_city,tmp-out_buffer)==0)
-		{
+		if (strncmp(out_buffer, home_city, tmp - out_buffer) == 0){
 		    app->current_station_id = g_strdup(ws->id_station);
 		    app->current_station_name = g_strdup(ws->name_station);
 		}
 	    }	    
 	}
-	fclose (clock_file);
+	fclose(clock_file);
     }
-    if (home_city)
+    if(home_city)
 	g_free(home_city);
-    if (remote_city)
+    if(remote_city)
 	g_free(remote_city);	
-}
-/*******************************************************************************/
-gboolean fill_station_inform( struct weather_station *ws){
-    FILE *stations_file;  
-    char state_name[21];
-/*  unsigned char out_buffer[1024]; */
-    char out_buffer[1024]; /* buffer for work with stations.txt files*/
-    char temp_station_name[21];
-    char temp_station_code[9];
-    int i;
-    /* Search state or province on country and add stations to combobox*/
-    if((stations_file = fopen(STATIONS_FILE,"r")) != NULL){
-	memset(state_name, 0, sizeof(state_name)); /* Clear temporary value */
-	while(!feof(stations_file)){
-	    memset(out_buffer, 0, sizeof(out_buffer)); /* Clear buffer */
-	    fgets(out_buffer, sizeof(out_buffer), stations_file); /* Read Next Line */
-	    memset(temp_station_name, 0, sizeof(temp_station_name)); /* Clear buffer */
-	    if( (strlen(out_buffer) > 27) && ((char)out_buffer[0] != '!') ){ /* Is it country or state or province name ? */
-    		if( out_buffer[19] != ' ' )
-		    sprintf(state_name,"%.19s",out_buffer);
-	/* Check for wrong string */ 
-		if( strlen(out_buffer) > 90 ){
-	  /* Prepare strings station_name and code_name for work */
-		    for(i = 3; i < 19; i++)
-			temp_station_name[i - 3] = out_buffer[i];
-		    for(i = 84; i < 92; i++)
-			temp_station_code[i - 84] = out_buffer[i];
-		    temp_station_code[8] = 0;
-	  /* Trim right space */
-    		    for(i = 15; i > 0; i--)
-    			if(temp_station_name[i] == ' ')
-        		    temp_station_name[i] = 0;
-    			else
-        		    break;
-		    if( strcmp(ws->id_station,temp_station_code) == 0 ){
-			ws->name_station = g_strdup(temp_station_name);	   
-			fclose(stations_file);  
-			return TRUE;
-		    } 
-		}    	  
-	    } 
-	}
-	fclose(stations_file);  
-    }
-    return FALSE;    
-}
-/*******************************************************************************/
-/* Reinitialize stations list */
-void reinitilize_stations_list(gchar *stations_string){
-    struct weather_station *ws;
-    gchar	*temp1 = NULL,
-		*temp2 = NULL;
-
-    stations_view_list = NULL; /* Initialize value */
-    temp1 = g_strdup(stations_string);
- /* Delimit stations Id string */
-    if(strlen(temp1) > 0){
-	temp2 = strtok(temp1, "@\0"); /* Delimiter between ID - @ */
-	if(temp2)  /* Check random error */      
-	    do{
-		if(strlen(temp2) > 0){
-		    ws = g_new0(struct weather_station, 1);
-		    ws->id_station = g_strdup(temp2);
-		    if(fill_station_inform(ws))
-			stations_view_list = g_slist_append(stations_view_list, ws); /* Add station to stations list */
-		    else{
-			g_free(ws->id_station);
-			g_free(ws);
-		    }
-		}
-		temp2 = strtok(NULL, "@\0"); /* Delimiter between ID - @ */
-	    }while(temp2);
-    }
-    g_free(temp1);       
 }
 /*******************************************************************************/
 /* New Reinitialize stations list */
@@ -227,24 +149,6 @@ void reinitilize_stations_list2(GSList *stlist){
 	g_free(temp1);
 	stlist = g_slist_next(stlist);
     }	    
-}
-/*******************************************************************************/
-/* Prepare stations ID list in one string to write config file */
-gchar* prepare_idlist_string(void){
-    GString *result_string;
-    GSList *tmplist = NULL;
-    struct weather_station *ws;
-
-   /* Initialize value */
-    tmplist = stations_view_list;
-    result_string = g_string_new(NULL);
-    while(tmplist != NULL){
-	ws = tmplist->data;
-	g_string_append(result_string, ws->id_station);
-	g_string_append_c(result_string,'@'); /* @ - delimiter */
-	tmplist = g_slist_next(tmplist);
-    }      
-    return g_string_free(result_string, FALSE);
 }
 /*******************************************************************************/
 /* Prepare stations ID list  to write config file */
@@ -298,8 +202,7 @@ void read_config(void){
 	g_free(home_dir);
     }
     else
-	snprintf(tmp_buff, sizeof(tmp_buff) - 1, "%s",
-		    tmp);
+	snprintf(tmp_buff, sizeof(tmp_buff) - 1, "%s", tmp);
     if(!config_set_weather_dir_name(gnome_vfs_expand_initial_tilde(tmp_buff)))
         fprintf(stderr, _("Could not create Weather Cache directory.\n"));
     if(tmp)
@@ -494,6 +397,190 @@ void read_config(void){
     g_object_unref(gconf_client);
 }
 /*******************************************************************************/
+int new_read_config(AppletConfig *config){
+    gchar	*tmp = NULL;
+    GConfValue	*value = NULL;
+    GConfClient *gconf_client = NULL;
+    int		fd = -1;
+    GSList	*stlist = NULL;
+    GError	*gerror = NULL;
+    GdkColor	DEFAULT_FONT_COLOR = {0, 0x0d00, 0x2a00, 0xc000};
+    gchar	tmp_buff[1024],
+		*home_dir;
+
+    gconf_client = gconf_client_get_default();
+
+    if(!gconf_client){
+        fprintf(stderr, _("Failed to initialize GConf. Quitting.\n"));
+        return 1;
+    }
+    /* Get Weather Cache Directory.  Default is "~/apps/omweather". */
+    tmp = gconf_client_get_string(gconf_client,
+        			    GCONF_KEY_WEATHER_DIR_NAME, NULL);
+    if(!tmp){
+	home_dir = getenv("HOME");
+	if(!home_dir){
+    	    fprintf(stderr, _("Can not get path to the HOME directory. Quitting.\n"));
+    	    return 1;
+	}
+	memset(tmp_buff, 0, sizeof(tmp_buff));
+	snprintf(tmp_buff, sizeof(tmp_buff) - 1, "%s%s",
+		    home_dir, "/apps/omweather");
+	g_free(home_dir);
+    }
+    else
+	snprintf(tmp_buff, sizeof(tmp_buff) - 1, "%s", tmp);
+    if(!config_set_weather_dir_name(gnome_vfs_expand_initial_tilde(tmp_buff)))
+        fprintf(stderr, _("Could not create Weather Cache directory.\n"));
+    if(tmp)
+	g_free(tmp);
+    tmp = NULL;
+    /* Get Weather Station ID for current station */
+    config->current_station_id = gconf_client_get_string(gconf_client,
+        			    GCONF_KEY_WEATHER_CURRENT_STATION_ID, NULL);
+    /* Get Weather Stations ID and NAME */
+    stlist = gconf_client_get_list(gconf_client,
+        			    GCONF_KEY_WEATHER_STATIONS_LIST,
+				    GCONF_VALUE_STRING, NULL);
+    if(stlist){
+	reinitilize_stations_list2(stlist);
+	g_slist_free(stlist);
+    }
+    /* Get icon set name */ 
+    config->icon_set = gconf_client_get_string(gconf_client,
+					    GCONF_KEY_WEATHER_ICON_SET,
+					    NULL);
+    sprintf(path_large_icon, "%s%s/", ICONS_PATH, config->icon_set);
+    if( ( fd = open(path_large_icon, O_RDONLY) ) == -1 ){
+	memset(path_large_icon, 0, sizeof(path_large_icon));
+	config->icon_set = g_strdup("Crystal");
+	sprintf(path_large_icon, "%s%s/", ICONS_PATH, config->icon_set);
+    }
+    else
+	close(fd);
+    /* Get Weather Icon Size  */		     
+    config->icons_size = gconf_client_get_int(gconf_client,
+        				    GCONF_KEY_WEATHER_ICONS_SIZE,
+					    NULL);
+    if(config->icons_size < 0)
+        config->icons_size = LARGE;
+    /* Get setting tab number  */		     
+    config->current_settings_page = gconf_client_get_int(gconf_client,
+        				    GCONF_KEY_WEATHER_SETTING_TAB_NUMBER,
+					    NULL);
+    if(config->current_settings_page < 0)
+        config->current_settings_page = 0;
+    /* Get Weather country name. */    
+    config->current_country = gconf_client_get_string(gconf_client,
+        					    GCONF_KEY_WEATHER_CURRENT_COUNTRY_NAME,
+						    NULL);
+    /* Get Weather station name. */
+    config->current_station_name = gconf_client_get_string(gconf_client,
+    							GCONF_KEY_WEATHER_CURRENT_STATION_NAME,
+							NULL);
+    /* Get Weather periodic update time . */
+    config->update_interval = gconf_client_get_int(gconf_client,
+        			    GCONF_KEY_WEATHER_UPDATE_INTERVAL,
+				    NULL);
+    if(config->update_interval < 0)
+	config->update_interval = 0;
+    /* Get Weather font color. */    	
+    tmp = gconf_client_get_string(gconf_client,
+        			    GCONF_KEY_WEATHER_FONT_COLOR, NULL);
+    if(!tmp || !gdk_color_parse(tmp, &(config->font_color)))
+         config->font_color = DEFAULT_FONT_COLOR; 
+    g_free(tmp);
+    tmp = NULL;
+    /* Get Enable Transparency flag. Default is TRUE. */
+    value = gconf_client_get(gconf_client, GCONF_KEY_ENABLE_TRANSPARENCY, NULL);
+    if(value){
+        config->transparency = gconf_value_get_bool(value);
+        gconf_value_free(value);
+    }
+    else
+        config->transparency = TRUE;      
+    /* Get Split Button State. Default is FALSE */
+    value = gconf_client_get(gconf_client, GCONF_KEY_SEPARATE_DATA, NULL);
+    if(value){
+        config->separate = gconf_value_get_bool(value);
+        gconf_value_free(value);
+    }
+    else
+        config->separate = FALSE;      	
+    /* Get Swap Temperature Button State. Default is FALSE */
+    value = gconf_client_get(gconf_client, GCONF_KEY_SWAP_TEMPERATURE, NULL);
+    if(value){
+        config->swap_hi_low_temperature = gconf_value_get_bool(value);
+        gconf_value_free(value);
+    }
+    else
+        config->swap_hi_low_temperature = FALSE;
+    /* Get Hide Station State. Default is FALSE */
+    value = gconf_client_get(gconf_client, GCONF_KEY_HIDE_STATION_NAME, NULL);
+    if(value){
+        config->hide_station_name = gconf_value_get_bool(value);
+        gconf_value_free(value);
+    }
+    else
+        config->hide_station_name = FALSE;
+    /* Get Temperature Unit  Default Celsius */
+    config->temperature_units = gconf_client_get_int(gconf_client,
+                    			GCONF_KEY_WEATHER_TEMPERATURE_UNIT, &gerror);
+    (config->temperature_units) ? (config->temperature_units = FAHRENHEIT)
+				: (config->temperature_units = CELSIUS);
+    if(gerror){
+    	g_error_free(gerror);
+	gerror = NULL;
+    }
+
+    /* Get Layout  Default Horizontal */
+    config->icons_layout = gconf_client_get_int(gconf_client,
+                				GCONF_KEY_ICONS_LAYOUT,
+						NULL);
+    if(config->icons_layout < 0)
+	config->icons_layout = ONE_ROW;
+    /* Get number days to show default 5 */
+    config->days_to_show = gconf_client_get_int(gconf_client,                                                                                     
+                				GCONF_KEY_WEATHER_DAYS,
+						&gerror);
+    if(gerror){
+	config->days_to_show = 5;
+	g_error_free(gerror);
+    }
+    if(config->days_to_show <= 0)
+	config->days_to_show = 5;
+	
+    config->previos_days_to_show = config->days_to_show;
+    /* Get distance units */
+    config->distance_units = gconf_client_get_int(gconf_client,                                                                                     
+                				    GCONF_KEY_WEATHER_DISTANCE_UNITS,
+						    &gerror);
+    if(gerror){
+	config->distance_units = METERS;
+	g_error_free(gerror);
+    }
+    /* Get wind units */
+    config->wind_units = gconf_client_get_int(gconf_client,                                                                                     
+						GCONF_KEY_WEATHER_WIND_UNITS,
+						&gerror);
+    if(gerror){
+	config->wind_units = METERS_S;
+	g_error_free(gerror);
+    }
+    /* Get valid data time for current weather */
+    config->data_valid_interval =
+	    gconf_client_get_int(gconf_client,
+				    GCONF_KEY_WEATHER_VALID_DATA_TIME,
+				    &gerror) * 3600;
+    if(gerror){
+	config->data_valid_interval = 2 * 3600; /* Default value - 2 hours */
+	g_error_free(gerror);
+    }
+    gconf_client_clear_cache(gconf_client);
+    g_object_unref(gconf_client);
+    return 0;
+}
+/*******************************************************************************/
 void config_update_proxy(void){
     GConfClient *gconf_client = gconf_client_get_default();
     if(app->iap_http_proxy_host)
@@ -515,7 +602,7 @@ void config_update_proxy(void){
 }
 /*******************************************************************************/
 /* Save current station position configuration data to GCONF. */
-void config_save_current_station(){
+void config_save_current_station(void){
     GConfClient *gconf_client = gconf_client_get_default();
     
     if(!gconf_client){
@@ -534,7 +621,7 @@ void config_save_current_station(){
 }
 /*******************************************************************************/ 
 /* Save all configuration data to GCONF. */
-void config_save(){
+void config_save(void){
     gchar temp_buffer[16];
     GConfClient *gconf_client;
     GSList *stlist = NULL;

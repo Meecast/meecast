81843740b5ebc3773512449796373be734eab6f8
pavel <pavel>
2007-08-22 12:33:20 +0000

    change fuction prototype
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1120 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-home_settings.h
+++ omweather/src/weather-home_settings.h
@@ -75,7 +75,7 @@ static GtkWidget *window_add_station;
 
 static char flag_update_station = FALSE; /* Flag update station list */
 static gchar *_weather_station_id_temp; /* Temporary value for weather_station_id */
-void create_icon_set_list(GtkWidget *store); /* scan /usr/share/weather/icons */
+int create_icon_set_list(GtkWidget *store); /* scan /usr/share/weather/icons */
 					     /* and add names to the list */
 void create_help_dialog(void);	/* create help dialog */
 GtkWidget* create_scrolled_window_with_text(const char* text,

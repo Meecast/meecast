0e741f39d6ef7c1d378bb9afcbcff5ebcdc76873
Vlad Vasilyeu <vasvlad@gmail.com>
2007-10-07 17:19:14 +0000

    fontsize
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1210 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-window_popup.c
+++ omweather/src/weather-window_popup.c
@@ -650,11 +650,11 @@ GtkWidget* create_temperature_range_widget(int i){
 			"%d\302\260%c", hi_temp, symbol);
     /* prepare temperature hbox */
     temperature_value_label = gtk_label_new(buffer);
-    set_font_size(temperature_value_label, 14);
+    set_font_size(temperature_value_label, 16);
     set_font_color(temperature_value_label, 0x0000, 0x0000, 0x0000);
     main_widget = gtk_hbox_new(FALSE, 10);
     temperature_title_label = gtk_label_new(_("Temperature: "));
-    set_font_size(temperature_title_label, 16);
+    set_font_size(temperature_title_label, 14);
     gtk_box_pack_start(GTK_BOX(main_widget), temperature_title_label, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(main_widget), temperature_value_label, FALSE, FALSE, 0);
     return main_widget;

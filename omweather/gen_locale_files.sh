b437597df5029e56fa931643857b752b8c17173b
Vlad Vasilyeu <vasvlad@gmail.com>
2007-11-02 16:16:25 +0000

    Next step
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1266 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/gen_locale_files.sh
+++ omweather/gen_locale_files.sh
@@ -7,3 +7,11 @@ for language in ru_RU fi_FI fr_FR en_GB en_US; do
     msgmerge -U $language.po omweather.pot
 done
 cd ..
+
+#xgettext -L Glade --keyword=_ -f data/help/POTFILES.in -s -C -o data/help/omweather_help.pot
+#cd data/help
+#for language in ru_RU fi_FI fr_FR en_GB en_US; do
+#    echo -n "$language: "
+#    msgmerge -U $language.po omweather_help.pot
+#done
+#cd ../..

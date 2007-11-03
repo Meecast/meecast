dce11c294b38a725143ff8da58ca9ccfd261a511
Vlad Vasilyeu <vasvlad@gmail.com>
2007-11-03 06:29:39 +0000

    PO and POT files
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1269 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/gen_locale_files.sh
+++ omweather/gen_locale_files.sh
@@ -9,6 +9,7 @@ done
 cd ..
 
 intltool-extract --type=gettext/xml data/help/omweather.xml.in
+xgettext --from-code=utf-8 --keyword=N_ -f data/help/po/POTFILES.in -s -C -o data/help/po/omweather.pot
 #xgettext -L Glade --keyword=_ -f data/help/POTFILES.in -s -C -o data/help/omweather_help.pot
 #cd data/help
 #for language in ru_RU fi_FI fr_FR en_GB en_US; do

567525a9940d8ee5c45bffd20731ed353c138c82
Vlad Vasilyeu <vasvlad@gmail.com>
2007-11-04 07:16:04 +0000

    The system of translation has been done
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1282 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/gen_locale_files.sh
+++ omweather/gen_locale_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ALL_LANGUAGES="ru_RU fi_FI fr_FR en_GB en_US"
+
 xgettext --keyword=_ -f po/POTFILES.in -s -C -o po/omweather.pot
 cd po
-for language in ru_RU fi_FI fr_FR en_GB en_US; do
+for language in $ALL_LANGUAGES; do
     echo -n "$language: "
     msgmerge -U $language.po omweather.pot
 done
@@ -10,9 +12,11 @@ cd ..
 
 intltool-extract --type=gettext/xml data/help/omweather.xml.in
 xgettext --from-code=utf-8 --keyword=N_ -f data/help/po/POTFILES.in -s -C -o data/help/po/omweather_help.pot
-#cd data/help
-#for language in en_GB en_US; do
-#    echo -n "$language: "
-#    msgmerge -U $language.po omweather_help.pot
-#done
-#cd ../..
+cd data/help/po
+for language in $ALL_LANGUAGES; do
+    echo -n "$language: "
+    msgmerge -U $language.po omweather_help.pot
+done
+cd ../..
+
+exit 0

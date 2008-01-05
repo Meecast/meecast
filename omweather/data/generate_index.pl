9e094f5ed9a07974597804a4ef86b21b3c15b968
pavel <pavel>
2008-01-05 17:42:44 +0000

    rename folder
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1451 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/data/generate_index.pl
+++ omweather/data/generate_index.pl
@@ -2,20 +2,20 @@
 
 use strict;
 
-my $source = "./countrys";
-my ($countrysfile, $regionsfile, $locationsfile) = ("countrys.list", "regions.list", "locations.list");
+my $source = "./countries";
+my ($countriesfile, $regionsfile, $locationsfile) = ("countries.list", "regions.list", "locations.list");
 
-open(COUNTRYS, ">$countrysfile") || die "Can't create file $countrysfile: $!\n";
+open(COUNTRIES, ">$countriesfile") || die "Can't create file $countriesfile: $!\n";
 open(REGIONS, ">$regionsfile") || die "Can't create file $regionsfile: $!\n";
 open(LOCATIONS, ">$locationsfile") || die "Can't create file $locationsfile: $!\n";
 
-my $countrys = countrylist($source);
-foreach my $country (sort(@$countrys)){
+my $countries = countrylist($source);
+foreach my $country (sort(@$countries)){
     my $region_start = undef;
     
     if(defined $region_start){
 	my $pos = tell(REGIONS);
-	print COUNTRYS "$country;$region_start;$pos;\n";
+	print COUNTRIES "$country;$region_start;$pos;\n";
 	$region_start = $pos;
     }
     else{
@@ -50,20 +50,20 @@ foreach my $country (sort(@$countrys)){
         }
     }
     if(defined $region_start){
-        print COUNTRYS "$country;$region_start;".scalar(tell(REGIONS) - 1).";\n";
+        print COUNTRIES "$country;$region_start;".scalar(tell(REGIONS) - 1).";\n";
     }
 }
 
-close COUNTRYS;
+close COUNTRIES;
 close REGIONS;
 close LOCATIONS;
 
 sub countrylist{
     my $path = shift;
-    my @countrys;
+    my @countries;
     
     opendir(DIR, $path) || die "Can't opendir $path: $!\n";
-    @countrys = grep {/^[^\.]/} readdir(DIR);
+    @countries = grep {/^[^\.]/} readdir(DIR);
     closedir DIR;
-    return \@countrys;
+    return \@countries;
 }

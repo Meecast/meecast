6ad46e56f55d4e0649ec6aa85bdb47715793281e
pavel <pavel>
2008-03-23 11:48:33 +0000

    add warning
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@1653 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-hash.c
+++ omweather/src/weather-hash.c
@@ -5,6 +5,13 @@ GHashTable* hash_table_create(void){
     GHashTable *hash;
     
     hash = g_hash_table_new(g_str_hash, g_str_equal);
+/*
+ * WARNING!
+ * Do not insert new lines to this file, use file hash.data
+ * To add new reserved word from data (xml) file you can use
+ * script get_reserved_word.pl like this:
+ * cat BOXX0014.xml USCA0001.xml >./get_reserved_word.pl
+*/
 #include "hash.data"
     return  hash;
 }

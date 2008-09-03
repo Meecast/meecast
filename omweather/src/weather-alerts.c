780cca6d7d84bb9acd317ce99aca328593f23d0a
pavel <pavel>
2008-09-03 12:14:12 +0000

    next step for alerts
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@2114 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-alerts.c
+++ omweather/src/weather-alerts.c
@@ -31,25 +31,52 @@
 /*******************************************************************************/
 GtkWidget* create_alerts_page(GtkWidget *window){
     GtkWidget	*main_widget = NULL,
-		*scrolled_window = NULL;
+		*scrolled_window = NULL,
+		*user_alerts_list_view = NULL,
+		*alerts_list_view = NULL,
+		*alerts_list_viwq = NULL,
+		*buttons_box = NULL,
+		*add_button = NULL,
+		*remove_button = NULL;
 #ifdef DEBUGFUNCTIONCALL
     START_FUNCTION;
 #endif
     main_widget = gtk_hbox_new(FALSE, 0);
+    /* User alerts list */
+    scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolled_window),
+					GTK_SHADOW_ETCHED_IN);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+                                 GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_widget_set_size_request(GTK_WIDGET(scrolled_window), 250, 280);
+    gtk_box_pack_start(GTK_BOX(main_widget), scrolled_window, FALSE, FALSE, 0);
+
+    user_alerts_list_view = create_tree_view(app->user_alerts_list);
+    GLADE_HOOKUP_OBJECT(window, user_alerts_list_view, "user_alerts_list_view");
+    gtk_container_add(GTK_CONTAINER(scrolled_window),
+			GTK_WIDGET(user_alerts_list_view));
+    /* buttons */
+    buttons_box = gtk_vbox_new(FALSE, 0);
+    add_button = create_button_with_image(NULL, "qgn_list_hw_button_left",
+                                	    26, TRUE, FALSE);
+    remove_button = create_button_with_image(NULL, "qgn_list_hw_button_right",
+                                	    26, TRUE, FALSE);
+    gtk_box_pack_start(GTK_BOX(buttons_box), add_button, FALSE, FALSE, 80);
+    gtk_box_pack_start(GTK_BOX(buttons_box), remove_button, FALSE, FALSE, 80);
+    gtk_box_pack_start(GTK_BOX(main_widget), buttons_box, FALSE, FALSE, 10);
     /* Alerts list */
     scrolled_window = gtk_scrolled_window_new(NULL, NULL);
     gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolled_window),
 					GTK_SHADOW_ETCHED_IN);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
                                  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-    gtk_widget_set_size_request(GTK_WIDGET(scrolled_window), 220, 280);
+    gtk_widget_set_size_request(GTK_WIDGET(scrolled_window), 250, 280);
     gtk_box_pack_start(GTK_BOX(main_widget), scrolled_window, FALSE, FALSE, 0);
-/*
-    alerts_list_view = create_tree_view(app->user_stations_list);
-    GLADE_HOOKUP_OBJECT(window, alerts_list_view, "alerts_list_view");
+
+    alerts_list_view = create_tree_view(create_alerts_list());
+    GLADE_HOOKUP_OBJECT(window, user_alerts_list_view, "alerts_list_view");
     gtk_container_add(GTK_CONTAINER(scrolled_window),
 			GTK_WIDGET(alerts_list_view));
-*/
     gtk_widget_show_all(main_widget);
     return main_widget;
 }
@@ -92,11 +119,25 @@ GSList* create_list_of_user_alerts(GtkListStore* list){
 	gtk_tree_model_get(GTK_TREE_MODEL(list), &iter, 
                     	    0, &alert_name,
                     	    -1);
-	str = g_strdup_(alert_name);
+	str = g_strdup(alert_name);
 	stlist = g_slist_append(stlist, str);
 	g_free(alert_name);
 	valid = gtk_tree_model_iter_next(GTK_TREE_MODEL(list), &iter);
     }
     return stlist;
+}
+/*******************************************************************************/
+GtkListStore* create_alerts_list(void){
+    GtkListStore	*list = NULL;
+    GtkTreeIter         iter;
+#ifdef DEBUGFUNCTIONCALL
+    START_FUNCTION;
+#endif
+    list = gtk_list_store_new(1, G_TYPE_STRING);
+    
+    gtk_list_store_append(list, &iter);
+    gtk_list_store_set(list, &iter, 0, _("Storm"), -1);
+
+    return list;
 }
 /*******************************************************************************/
--- omweather/src/weather-alerts.h
+++ omweather/src/weather-alerts.h
@@ -35,5 +35,6 @@ GtkWidget* create_alerts_page(GtkWidget *window);
 GtkListStore* create_user_alerts_list(void);
 void fill_user_alerts_list(GSList *source_list, GtkListStore** list);
 GSList* create_list_of_user_alerts(GtkListStore* list);
+GtkListStore* create_alerts_list(void);
 /*******************************************************************************/
 #endif

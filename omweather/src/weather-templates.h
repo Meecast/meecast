37c715b91ab8f1e2660843e154e1b137f32b7547
pavel <pavel>
2008-09-14 15:47:11 +0000

    new files
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@2125 5aa3d57c-301e-0410-a7f2-8fec96c11689

---
+++ omweather/src/weather-templates.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Other Maemo Weather(omweather)
+ *
+ * Copyright (C) 2006 Vlad Vasiliev
+ * Copyright (C) 2006 Pavel Fialko
+ * 	for the code
+ *        
+ * Copyright (C) 2008 Andrew Zhilin
+ *		      az@pocketpcrussia.com 
+ *	for default icon set (Glance)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _weather_templates_h
+#define _weather_templates_h 1
+/*******************************************************************************/
+#include "weather-common.h"
+/*******************************************************************************/
+GtkWidget* parse_template_from_file(const gchar *filename, const gchar *encoding);
+void parse_children(xmlNode *node, GtkWidget *object);
+/*******************************************************************************/
+extern void set_font(GtkWidget *widget, const gchar *description, const gint size);
+/*******************************************************************************/
+#endif
+

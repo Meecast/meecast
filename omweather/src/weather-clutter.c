6cde4a0c50c1f1149b082d19c56f4b0e739d9b0e
Vlad Vasilyeu <vasvlad@gmail.com>
2009-04-03 10:50:51 +0000

    Fixed problem with building
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@2518 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/src/weather-clutter.c
+++ omweather/src/weather-clutter.c
@@ -25,9 +25,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
 */
+#include "weather-clutter.h"
 #ifdef CLUTTER
 /*******************************************************************************/
-#include "weather-clutter.h"
 /*******************************************************************************/
 /* Timeline handler */
 void
@@ -64,4 +64,38 @@ show_animation(void){
     }
 }
 /*******************************************************************************/
+GtkWidget *
+create_clutter_main_icon(GdkPixbuf *icon_buffer, int icon_size)
+{
+    GtkWidget *icon_widget;
+    SuperOH *oh;
+    GtkWidget *clutter;
+    ClutterColor stage_color;
+
+    stage_color.red = app->config->background_color.red;
+    stage_color.blue = app->config->background_color.blue;
+    stage_color.green = app->config->background_color.green;
+    stage_color.alpha = 0xff;
+
+    oh = g_new(SuperOH, 1);
+    icon_widget = gtk_vbox_new(FALSE, 0);
+    clutter = gtk_clutter_embed_new();
+    gtk_widget_set_size_request (clutter, icon_size, icon_size);
+    gtk_container_add (GTK_CONTAINER (icon_widget), clutter);
+    oh->stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (clutter));
+    /* and its background color */
+    clutter_stage_set_color (CLUTTER_STAGE (oh->stage),
+                  &stage_color);
+    /* create a new group to hold multiple actors in a group */
+    oh->group = CLUTTER_GROUP (clutter_group_new ());
+    oh->icon = gtk_clutter_texture_new_from_pixbuf (icon_buffer);
+    clutter_group_add (oh->group, oh->icon);
+    /* Add the group to the stage */
+    clutter_container_add_actor (CLUTTER_CONTAINER (oh->stage),
+                               CLUTTER_ACTOR (oh->group));
+    app->clutter_objects_list = g_slist_append(app->clutter_objects_list, oh);
+    /* !!!!!!!!!! */
+    clutter_actor_show_all (CLUTTER_ACTOR (oh->group));
+    return icon_widget;
+}
 #endif

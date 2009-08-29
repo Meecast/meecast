b9c3499e09bb45a94ec8ca438537717638684103
Vlad Vasilyeu <vasvlad@gmail.com>
2009-08-29 18:09:26 +0000

    probing
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3163 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-gismeteo-ru-stations-db/data/download_station.py
+++ omweather-gismeteo-ru-stations-db/data/download_station.py
@@ -6,6 +6,7 @@ import urllib
 import sqlite3 as db
 import libxml2, sys
 import os
+import re
 
 
 base_url = "http://wap.gismeteo.ru/gm/normal/node/select_country_city/6/?field__assoc_4_country=%s"
@@ -16,11 +17,17 @@ cu = c.cursor()
 
 #Search  id of country
 cur = cu.execute("select distinct id_gismeteo_old from countries")
-# Main cicle
+
+myrow = []
 for row in cur:
+    myrow += [row[0]]
+c.commit()
+
+# Main cicle
+for row in myrow:
 
     #download list of stations
-    country_id = row[0]
+    country_id = row
     print country_id
     mysock = urllib.urlopen(base_url % country_id)
     fileToSave = mysock.read()
@@ -36,6 +43,7 @@ for row in cur:
     iFile.close
     os.remove(r"./%s" % country_id)
 
+    a = re.compile('\d\d\d+')
     #parse xml file
     doc = libxml2.htmlReadFile(r"./%s.xml" % country_id, "UTF-8", libxml2.HTML_PARSE_RECOVER)
     ctxt = doc.xpathNewContext()
@@ -43,10 +51,18 @@ for row in cur:
     for anchor in anchors:
         href = anchor.prop("href")
         if '/gm/normal/node/prognoz_type/6/?field_wmo' in href:
-            print href, " ", anchor.content
+            sql_string = 'insert into stations (region_id, russian_name, name, id_gismeteo_old, code) values ((select distinct regions.id from regions,countries where regions.country_id=countries.id and countries.id_gismeteo_old = %s), "%s", "%s", %s, %s)'  % (country_id , anchor.content , anchor.content, a.search(href).group(), a.search(href).group())
+            print sql_string
+            cu.execute(sql_string)
+            c.commit()
+            print sql_string
+#            print href, " ", anchor.content
+#            print a.search(href).group()
+
 
     doc.freeDoc()
+    os.remove(r"./%s.xml" % country_id)
 
-c.commit()
+
+#c.commit()
 c.close()
-

6f7f062869d1143c567bd345b18f03f97154d695
Vlad Vasilyeu <vasvlad@gmail.com>
2009-08-29 16:35:11 +0000

    next step for script
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3162 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-gismeteo-ru-stations-db/data/download_station.py
+++ omweather-gismeteo-ru-stations-db/data/download_station.py
@@ -4,6 +4,9 @@
 #from urllib import urlopen
 import urllib
 import sqlite3 as db
+import libxml2, sys
+import os
+
 
 base_url = "http://wap.gismeteo.ru/gm/normal/node/select_country_city/6/?field__assoc_4_country=%s"
 
@@ -15,7 +18,8 @@ cu = c.cursor()
 cur = cu.execute("select distinct id_gismeteo_old from countries")
 # Main cicle
 for row in cur:
-    #download list
+
+    #download list of stations
     country_id = row[0]
     print country_id
     mysock = urllib.urlopen(base_url % country_id)
@@ -23,6 +27,25 @@ for row in cur:
     oFile = open(r"./%s" % country_id,'wb')
     oFile.write(fileToSave)
     oFile.close
+
+    #normalize file
+    oFile = open(r"./%s" % country_id,'r')
+    iFile = open(r"./%s.xml" % country_id,'w')
+    iFile.write(oFile.read().replace("&", "&amp;"))
+    oFile.close
+    iFile.close
+    os.remove(r"./%s" % country_id)
+
+    #parse xml file
+    doc = libxml2.htmlReadFile(r"./%s.xml" % country_id, "UTF-8", libxml2.HTML_PARSE_RECOVER)
+    ctxt = doc.xpathNewContext()
+    anchors = ctxt.xpathEval("//body/div/a")
+    for anchor in anchors:
+        href = anchor.prop("href")
+        if '/gm/normal/node/prognoz_type/6/?field_wmo' in href:
+            print href, " ", anchor.content
+
+    doc.freeDoc()
 
 c.commit()
 c.close()

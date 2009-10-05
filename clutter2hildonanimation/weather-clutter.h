7fc8ef1f2454712440300042e1035712ab8f8786
Vlad Vasilyeu <vasvlad@gmail.com>
2009-10-05 19:29:52 +0000

    new branch for clutter and hildonanimation
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3344 5aa3d57c-301e-0410-a7f2-8fec96c11689

---
+++ clutter2hildonanimation/weather-clutter.h
@@ -0,0 +1,39 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Other Maemo Weather(omweather)
+ *
+ * Copyright (C) 2006-2009 Vlad Vasiliev
+ * Copyright (C) 2006-2009 Pavel Fialko
+ * 	for the code
+ *        
+ * Copyright (C) 2009 Andrew Zhilin
+ *		      az@pocketpcrussia.com 
+ *	for default icon set (Glance)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _weather_clutter_h
+#define _weather_clutter_h 1
+/*******************************************************************************/
+
+#include <clutter/clutter.h>
+#include <hildon/hildon.h>
+#include <gtk/gtk.h>
+GtkWidget *create_hildon_clutter_icon_animation(const char *icon_path, int icon_size, GSList **objects_list);
+
+/*******************************************************************************/
+#endif
---
+++ clutter2hildonanimation/weather-main.c
@@ -0,0 +1,53 @@
+/* vim: set sw=4 ts=4 et: */
+/* This file is part of Other Maemo Weather(omweather)
+ *
+ * Copyright (C) 2006-2009 Vlad Vasiliev
+ * Copyright (C) 2006-2009 Pavel Fialko
+ * 	for the code
+ *
+ * Copyright (C) 2008 Andrew Zhilin
+ *		      az@pocketpcrussia.com 
+ *	for default icon set (Glance)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+*/
+/*******************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include "weather-main.h"
+/*******************************************************************************/
+int
+main(int argc, char *argv[]){
+    GtkWidget* window;
+    GSList              *clutter_objects_in_main_form = NULL;
+#ifdef ENABLE_NLS
+    setlocale(LC_ALL, "");
+    bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
+#endif
+    gtk_init(&argc, &argv);
+
+    clutter_init(NULL, NULL);
+    create_hildon_clutter_icon_animation("/usr/share/omweather/icons/Glance/0.png", 128, &clutter_objects_in_main_form);
+#ifdef DEBUGFUNCTIONCALL
+    END_FUNCTION;
+#endif
+    return 0;
+}
+

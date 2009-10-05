64ad29a58078d17037f7f5210091f6338984a399
pavel <pavel>
2009-10-05 09:38:15 +0000

    fixed warrning
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3336 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-core/src/omweather-config.cpp
+++ omweather-core/src/omweather-config.cpp
@@ -388,7 +388,7 @@ void Config::prepare_save(){
     if(document)
         xmlFreeDoc(document);
     document = xmlNewDoc(BAD_CAST "1.0");
-    xmlNs *ns = xmlNewGlobalNs(document, BAD_CAST "https://garage.maemo.org/projects/omweather/", BAD_CAST "");
+    xmlNewGlobalNs(document, BAD_CAST "https://garage.maemo.org/projects/omweather/", BAD_CAST "");
     root_node = xmlNewNode(NULL, BAD_CAST "omweather");
 }
 /*******************************************************************************/

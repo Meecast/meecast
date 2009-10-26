47bde58f89261d3e7bcac03214b8234f02d4cf7e
pavel <pavel>
2009-10-26 12:50:16 +0000

    added new weather data structure
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3446 5aa3d57c-301e-0410-a7f2-8fec96c11689

---
+++ omweather-core/src/omweather-data.hpp
@@ -0,0 +1,47 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Other Maemo Weather(omweather)
+ *
+ * Copyright (C) 2006-2009 Vlad Vasiliev
+ * Copyright (C) 2006-2009 Pavel Fialko
+ *  for the code
+ *        
+ * Copyright (C) 2008 Andrew Zhilin
+ *            az@pocketpcrussia.com 
+ *  for default icon set (Glance)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _omweather_data_hpp
+#define _omweather_data_hpp 1
+/*******************************************************************************/
+#include <map>
+#include <vector>
+#include <string>
+/*******************************************************************************/
+typedef std::map<std::string,std::string> Location, Current, Day, Detail;
+typedef std::vector<Day> Days, Details;
+/*******************************************************************************/
+class OMWeatherData{
+    public:
+        Location location;
+        Current current;
+        Days days;
+        Details details;
+};
+/*******************************************************************************/
+#endif

162be89c32687f5823a2aacb2b68094764188b04
pavel <pavel>
2010-01-28 09:55:06 +0000

    added Bulgarian language
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3718 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/gen_locale_files.sh
+++ omweather/gen_locale_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALL_LANGUAGES="ru_RU fi_FI fr_FR en_GB en_US it_IT de_DE es_ES es_MX pl_PL"
+ALL_LANGUAGES="ru_RU fi_FI fr_FR en_GB en_US it_IT de_DE es_ES es_MX pl_PL bg_BG"
 
 xgettext --from-code=UTF-8 --keyword=_ --keyword=Q_ -f po/POTFILES.in -s -C -o po/omweather.pot
 cd po

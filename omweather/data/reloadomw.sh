dd6aedb65a6f0c3d84344b74783aaae9b45a488d
Vlad Vasilyeu <vasvlad@gmail.com>
2010-03-02 15:39:33 +0000

    removed
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3828 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather/data/reloadomw.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#Reload omweather plugin
-cat /home/user/.config/hildon-desktop/home.plugins|  grep -v "X-Desktop-File=/usr/share/applications/hildon-home/omweather-home.desktop"|grep -v "\[omweather-home.desktop-0\]" >/home/user/.config/hildon-desktop/home.plugins
-sleep 2 
-echo "[omweather-home.desktop-0]">>/home/user/.config/hildon-desktop/home.plugins
-echo "X-Desktop-File=/usr/share/applications/hildon-home/omweather-home.desktop">>/home/user/.config/hildon-desktop/home.plugins

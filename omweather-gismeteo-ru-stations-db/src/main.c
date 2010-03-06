f22c3e3a0c0ddd577c4b1bae22ad7363cbbac14d
Vlad Vasilyeu <vasvlad@gmail.com>
2010-03-06 16:20:29 +0000

    changed moon phase
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@3833 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-gismeteo-ru-stations-db/src/main.c
+++ omweather-gismeteo-ru-stations-db/src/main.c
@@ -735,19 +735,19 @@ fill_current_data(xmlNode *root_node, GHashTable *current_weather, GHashTable *d
                                                                                                 g_strdup("Full"));
                                                     }
                                                     if (strstr(temp_xml_string,"vax1")){
-                                                        if (!strncmp(temp_xml_string,"moon1",5)||!strncmp(temp_xml_string,"moon2",5))
+                                                        if (!strncmp(temp_xml_string,"moon1",5))
                                                             g_hash_table_insert(current_weather, "moon_phase", 
                                                                                                 g_strdup("New"));
                                                         if (!strncmp(temp_xml_string,"moon2",5)||!strncmp(temp_xml_string,"moon3",5))
                                                             g_hash_table_insert(current_weather, "moon_phase", 
                                                                                                 g_strdup("Waning Crescent"));
-                                                        if (!strncmp(temp_xml_string,"moon4",5)||!strncmp(temp_xml_string,"moon5",5))
+                                                        if (!strncmp(temp_xml_string,"moon3",5)||!strncmp(temp_xml_string,"moon4",5))
                                                             g_hash_table_insert(current_weather, "moon_phase", 
                                                                                                 g_strdup("Last Quarter"));
-                                                        if (!strncmp(temp_xml_string,"moon6",5)||!strncmp(temp_xml_string,"moon7",5))
+                                                        if (!strncmp(temp_xml_string,"moon5",5)||!strncmp(temp_xml_string,"moon6",5))
                                                             g_hash_table_insert(current_weather, "moon_phase", 
                                                                                                 g_strdup("Waning Gibbous"));
-                                                        if (!strncmp(temp_xml_string,"moon8",5))
+                                                        if (!strncmp(temp_xml_string,"moon7",5) || !strncmp(temp_xml_string,"moon8",5))
                                                             g_hash_table_insert(current_weather, "moon_phase", 
                                                                                                 g_strdup("Full"));
                                                     }

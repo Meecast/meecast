e78edc3a2c28b8cd28866cabb4e3e4f036b93f22
Vlad Vasilyeu <vasvlad@gmail.com>
2010-10-04 18:57:28 +0000

    next step for develop
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4030 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/sources.cpp
+++ qtomweather/sources.cpp
@@ -4,25 +4,26 @@
 #include <sstream>
 using namespace std;
 
-extern "C" {
-typedef void (*) (*parser_proto)(const gchar*, GHashTable*, gboolean);
+
+typedef int (*parser_proto)(const gchar*, GHashTable*, gboolean);
 
 void
 test (void){
     void *handle;
     GHashTable  *station_data;
     parser_proto parser;
+    int result;
 
-
+    station_data = g_hash_table_new(g_str_hash, g_str_equal);
 
     handle = dlopen("/usr/lib/omweather/libomweather-weather-com-source.so", RTLD_NOW);
     if(handle){
         dlerror();
-        parser = dlsym(handle, "get_station_weather_data");
+        parser = (parser_proto)dlsym(handle, "get_station_weather_data");
         if(!dlerror()){
-            cout<<"parser "<< parser<<endl;
-            parser("/tmp/test.xml", station_data, TRUE);
+            result = parser("/scratchbox/users/vlad/home/vlad/apps/omweather/BOXX0014.xml", station_data, FALSE);
+            cout<<"result "<<result<<endl;
         }
     }
 }
-}
+

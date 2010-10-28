0a415f1395fabc8860481650216244d55a8f386c
Vlad Vasilyeu <vasvlad@gmail.com>
2010-10-28 06:09:25 +0000

    next step for test
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4040 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/core/test.cpp
+++ qtomweather/core/test.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+
+#include "data.h"
 using namespace std;
+
+int
+test_class_data()
+{
+    Core::Data wdata;
+    delete wdata;
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-  cout<<"Test"<<endl;
+  cout<<"Testing..."<<endl;
+  cout<<"First test 1. Class Data   ";
+  if (test_class_data() == 0)
+      cout<<"Success"<<endl;
+  else
+      cout<<"Not Success"<<endl;
 }

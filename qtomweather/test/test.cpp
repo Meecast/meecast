126f212be5e5622cd7191e04f8f34e6251753c3c
pavel <pavel>
2010-11-03 13:43:41 +0000

    changes for new class
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4074 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/test.cpp
+++ qtomweather/test/test.cpp
@@ -11,7 +11,7 @@ create_and_fill_class_data_for_hours_forecast()
     Core::Data *wdata = new Core::Data;
     wdata->StartTime(time(NULL) - 3600);
     wdata->EndTime(time(NULL) + 3600);
-    wdata->Temperature(23.0);
+    wdata->temperature().value(23.0);
     wdata->Flike(18.0);
     wdata->WindSpeed(3.0);
     wdata->WindGust(4.0);
@@ -42,7 +42,7 @@ create_and_fill_class_data_for_day_forecast()
     end_current_day = mktime(tm);
     wdata->StartTime(begin_current_day);
     wdata->EndTime(end_current_day);
-    wdata->Temperature(20.0);
+    wdata->temperature().value(20.0);
     wdata->Flike(17.0);
     wdata->WindGust(2.8);
     wdata->WindSpeed(2.4);
@@ -75,7 +75,7 @@ test_class_datalist(){
     /* Check correct working of method GetDataForTime */
     wdata = wdata_list->GetDataForTime(time(NULL));
     if (!((wdata) &&
-          (wdata->Temperature() == 23.0)&&
+          (wdata->temperature().value() == 23.0)&&
           (wdata->Flike() == 18.0)&&
           (wdata->WindSpeed() == 3.0)&&
           (wdata->WindDirection().compare("NNW") == 0)&&

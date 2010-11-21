01c0a62c34f97da5aba77074d38f9b215e5d0c2b
Vlad Vasilyeu <vasvlad@gmail.com>
2010-11-21 16:39:57 +0000

    fixed test
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4108 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/run_test.sh
+++ qtomweather/run_test.sh
@@ -2,4 +2,7 @@
 qmake -recursive
 make
 export LD_LIBRARY_PATH=$PWD/core:$LD_LIBRARY_PATH
+cd test
+make 
+cd ..
 test/omweathertest
--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -5,7 +5,8 @@
 class OmweatherTest : public CxxTest::TestSuite{
     public:
         void testSourceClass(void){
-            Core::Source *s = new Core::Source("source.xml");
+ //           Core::Source *s = new Core::Source("source.xml");
+            Core::Source *s = new Core::Source();
             TS_ASSERT( 1 + 1 > 1 );
             TS_ASSERT_EQUALS( 1 + 1, 2 );
         }

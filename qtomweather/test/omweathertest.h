e4bd70e7debc3bd79d841053af8d36934ec0197f
pavel <pavel>
2010-11-22 06:58:25 +0000

    fixed error
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4109 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -5,8 +5,7 @@
 class OmweatherTest : public CxxTest::TestSuite{
     public:
         void testSourceClass(void){
- //           Core::Source *s = new Core::Source("source.xml");
-            Core::Source *s = new Core::Source();
+/           Core::Source *s = new Core::Source("source.xml");
             TS_ASSERT( 1 + 1 > 1 );
             TS_ASSERT_EQUALS( 1 + 1, 2 );
         }

dcae03db2ec68e703a97ce1b8e8f4cb93a6c8777
pavel <pavel>
2010-11-26 15:04:25 +0000

    Tests for name and logo
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4123 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -6,6 +6,9 @@ class OmweatherTest : public CxxTest::TestSuite{
     public:
         void testSourceClass(void){
 //default            Core::Source *s = new Core::Source("source.xml");
-            TS_ASSERT_THROWS_NOTHING(Core::Source *s = new Core::Source("source.xml", "../core/data/source.xsd") );
+            Core::Source *s = NULL;
+            TS_ASSERT_THROWS_NOTHING(s = new Core::Source("source.xml", "../core/data/source.xsd") );
+            TS_ASSERT(s->name().compare("weather.com") == 0);
+            TS_ASSERT(s->logo().compare("weather.com.png") == 0);
         }
 };

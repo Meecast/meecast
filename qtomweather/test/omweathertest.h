e5925d041426dc117bbf29790efbc6cf12f94f5c
Vlad Vasilyeu <vasvlad@gmail.com>
2010-11-26 14:24:16 +0000

    redisign test for Source
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4120 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -6,8 +6,6 @@ class OmweatherTest : public CxxTest::TestSuite{
     public:
         void testSourceClass(void){
 //default            Core::Source *s = new Core::Source("source.xml");
-            Core::Source *s = new Core::Source("source.xml", "../core/data/source.xsd");
-            TS_ASSERT( 1 + 1 > 1 );
-            TS_ASSERT_EQUALS( 1 + 1, 2 );
+            TS_ASSERT_THROWS_NOTHING( new Core::Source("source.xml", "../core/data/source.xsd") );
         }
 };

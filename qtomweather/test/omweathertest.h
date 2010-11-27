3133675959923965981c96b0fd79af103d02ec56
Vlad Vasilyeu <vasvlad@gmail.com>
2010-11-27 10:27:30 +0000

    Adedd negative test
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4125 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -7,6 +7,8 @@ class OmweatherTest : public CxxTest::TestSuite{
         void testSourceClass(void){
 //default            Core::Source *s = new Core::Source("source.xml");
             Core::Source *s = NULL;
+            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("null_source.xml", "../core/data/source.xsd") );
+            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("source.xml", "../core/data/null_source.xsd") );
             TS_ASSERT_THROWS_NOTHING(s = new Core::Source("source.xml", "../core/data/source.xsd") );
             TS_ASSERT(s->name().compare("weather.com") == 0);
             TS_ASSERT(s->logo().compare("weather.com.png") == 0);

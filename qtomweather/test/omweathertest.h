ce07309286a0d30b7f28f4158215de5c9737ea40
Vlad Vasilyeu <vasvlad@gmail.com>
2010-11-27 11:09:25 +0000

    Begun add test for Data
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4127 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -24,4 +24,60 @@ class OmweatherTest : public CxxTest::TestSuite{
             TS_ASSERT(c->TemperatureUnit().compare("F")== 0);
             TS_ASSERT(c->FontColor().compare("#00ff00")== 0);
         }
+        void testDataClass(void){
+            Core::Data *d = NULL;
+            TS_ASSERT_THROWS_NOTHING(d = create_and_fill_class_data_for_day_forecast());
+        }
+
+    private:
+        //////////////////////////////////////////////////////////////////////////////
+        Core::Data *
+        create_and_fill_class_data_for_hours_forecast()
+        {
+            Core::Data *wdata = new Core::Data;
+            wdata->StartTime(time(NULL) - 3600);
+            wdata->EndTime(time(NULL) + 3600);
+            wdata->temperature_hi().value(23.0);
+            wdata->temperature_low().value(15.0);
+            wdata->Flike().value(18.0);
+            wdata->WindSpeed(3.0);
+            wdata->WindGust(4.0);
+            wdata->WindDirection(std::string("NNW"));
+            wdata->Icon(3);
+            wdata->Text(std::string("Clear"));
+            return wdata;
+        }
+        //////////////////////////////////////////////////////////////////////////////
+        Core::Data *
+        create_and_fill_class_data_for_day_forecast()
+        {
+            time_t      current_time, begin_current_day, end_current_day;
+            struct tm   *tm = NULL;
+            int         year, current_month;
+
+            Core::Data *wdata = new Core::Data;
+            /* Create current day */
+            current_time = time(NULL);
+            begin_current_day = current_time;
+            tm = localtime(&begin_current_day);
+            year = 1900 + tm->tm_year;
+            current_month = tm->tm_mon;
+            tm->tm_sec = 0; tm->tm_min = 0; tm->tm_hour = 0;
+            tm->tm_isdst = 1;
+            begin_current_day = mktime(tm);
+            tm->tm_sec = 0; tm->tm_min = 59; tm->tm_hour = 23;
+            end_current_day = mktime(tm);
+            wdata->StartTime(begin_current_day);
+            wdata->EndTime(end_current_day);
+            wdata->temperature_low().value(20.0);
+            wdata->temperature_hi().value(10.0);
+            wdata->Flike().value(17.0);
+            wdata->WindGust(2.8);
+            wdata->WindSpeed(2.4);
+            wdata->Humidity(845);
+            wdata->Icon(4);
+            wdata->Text(std::string("Cloudy"));
+            return wdata;
+        }
+        //////////////////////////////////////////////////////////////////////////////
 };

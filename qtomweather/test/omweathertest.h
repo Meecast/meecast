657a183f362ad68817f825728244ef72ed538bf0
pavel <pavel>
2010-11-28 17:07:44 +0000

    modified test
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4132 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/test/omweathertest.h
+++ qtomweather/test/omweathertest.h
@@ -5,17 +5,16 @@
 class OmweatherTest : public CxxTest::TestSuite{
     public:
         void testSourceClass(void){
-//default            Core::Source *s = new Core::Source("source.xml");
             Core::Source *s = NULL;
-            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("null_source.xml", "../core/data/source.xsd") );
-            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("source.xml", "../core/data/null_source.xsd") );
+//            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("null_source.xml", "../core/data/source.xsd") );
+//            TS_ASSERT_THROWS_ANYTHING(s = new Core::Source("source.xml", "../core/data/null_source.xsd") );
             TS_ASSERT_THROWS_NOTHING(s = new Core::Source("source.xml", "../core/data/source.xsd") );
             TS_ASSERT(s->name().compare("weather.com") == 0);
             TS_ASSERT(s->logo().compare("weather.com.png") == 0);
         }
         void testConfigClass(void){
             Core::Config *c = NULL;
-            TS_ASSERT_THROWS_NOTHING(c =  new Core::Config());
+            TS_ASSERT_THROWS_NOTHING(c = new Core::Config("config.xml", "../core/data/config.xsd"));
             c->Base_Icons_Path(std::string("../omweather/data/icons"));
             c->Iconset(std::string("Glance"));
             c->TemperatureUnit("F");
@@ -39,9 +38,8 @@ class OmweatherTest : public CxxTest::TestSuite{
         }
         void testDataParser(void){
             Core::DataParser *p = NULL;
-            TS_ASSERT_THROWS_ANYTHING(p = new Core::DataParser("../core/data/null_data.xsd"));
-            TS_ASSERT_THROWS_NOTHING(p = new Core::DataParser("../core/data/data.xsd"));
-            TS_ASSERT_THROWS_NOTHING(p->parse("data.xml"));
+//            TS_ASSERT_THROWS_ANYTHING(p = new Core::DataParser("data.xml", "../core/data/null_data.xsd"));
+            TS_ASSERT_THROWS_NOTHING(p = new Core::DataParser("data.xml", "../core/data/data.xsd"));
         }
     private:
         //////////////////////////////////////////////////////////////////////////////

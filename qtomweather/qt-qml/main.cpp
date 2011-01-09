ec487cf02abdc6a6ef9c9720c3df0c8f66fe984e
Vlad Vasilyeu <vasvlad@gmail.com>
2011-01-09 08:24:45 +0000

    removed unusable code
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4211 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/qt-qml/main.cpp
+++ qtomweather/qt-qml/main.cpp
@@ -19,55 +19,6 @@
 #include "datamodel.h"
 
 //////////////////////////////////////////////////////////////////////////////
-DataQml *
-create_and_fill_class_data_for_day_forecast()
-{
-    time_t      current_time, begin_current_day, end_current_day;
-    struct tm   *tm = NULL;
-    int         year, current_month;
-
-    DataQml *wdata = new DataQml;
-    /* Create current day */
-    current_time = time(NULL);
-    begin_current_day = current_time;
-    tm = localtime(&begin_current_day);
-    year = 1900 + tm->tm_year;
-    current_month = tm->tm_mon;
-    tm->tm_sec = 0; tm->tm_min = 0; tm->tm_hour = 0;
-    tm->tm_isdst = 1;
-    begin_current_day = mktime(tm);
-    tm->tm_sec = 0; tm->tm_min = 59; tm->tm_hour = 23;
-    end_current_day = mktime(tm);
-    wdata->StartTime(begin_current_day);
-    wdata->EndTime(end_current_day);
-    wdata->Data::temperature_low().value(20.0);
-    wdata->Data::temperature_hi().value(30.0);
-    wdata->Flike().value(17.0);
-    wdata->WindGust(2.8);
-    wdata->WindSpeed(2.4);
-    wdata->Humidity(845);
-    wdata->Icon(4);
-    wdata->Text(std::string("Cloudy"));
-    return wdata;
-}
-//////////////////////////////////////////////////////////////////////////////
-
-DataItem *
-create_and_fill_class_data_for_hours_forecast()
-{
-    DataItem *wdata = new DataItem;
-    wdata->StartTime(time(NULL) - 3600);
-    wdata->EndTime(time(NULL) + 3600);
-    wdata->Data::temperature_hi().value(23.0);
-    wdata->Flike().value(18.0);
-    wdata->WindSpeed(3.0);
-    wdata->WindGust(4.0);
-    wdata->WindDirection(std::string("NNW"));
-    wdata->Icon(0);
-    wdata->Text(std::string("Clear"));
-    return wdata;
-}
-//////////////////////////////////////////////////////////////////////////////
 
 ConfigQml *
 create_and_fill_config(){
@@ -131,7 +82,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    forecast_data = create_and_fill_class_data_for_hours_forecast();
+
     DataModel *model = new DataModel(new DataItem, qApp);
     i = 0;
     while  (temp_data = dp->data().GetDataForTime(time(NULL) + i)) {

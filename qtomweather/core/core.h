524f61f601d2ca6542aa1396d418f072f242b8e9
Vlad Vasilyeu <vasvlad@gmail.com>
2011-03-23 09:20:00 +0000

    updated icons
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4652 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/core/core.h
+++ qtomweather/core/core.h
@@ -48,7 +48,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define OMWEATHER_VERSION "0.3.14"
+#define OMWEATHER_VERSION "0.3.16"
 namespace Core{
     class Connection;
     class AbstractConfig;

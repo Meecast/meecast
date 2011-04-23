c24e287cfe7794f64feb4c38ef2056085bcced6e
Vlad Vasilyeu <vasvlad@gmail.com>
2011-04-23 19:22:57 +0000

    next step for first start
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4735 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-yr-no-station-db/src/main.h
+++ omweather-yr-no-station-db/src/main.h
@@ -2,11 +2,12 @@
 /*
  * This file is part of omweather-weather-com-source
  *
- * Copyright (C) 2011 Vlad Vasiliev
+ * Copyright (C) 2006-2009 Vlad Vasiliev
+ * Copyright (C) 2006-2009 Pavel Fialko
  * 	for the code
  *
  * This software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
+ * modify it under the terms of the GNU  General Public License
  * as published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
  *
@@ -26,21 +27,13 @@
 /*******************************************************************************/
 #include <gtk/gtk.h>
 #include <libxml/parser.h>
-#include <libxml/HTMLparser.h>
-#include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
 #include <stdlib.h>
-#include <glib.h>
-#include <locale.h>
 /*******************************************************************************/
+gboolean source_init(void);
+void source_destroy(void);
+void free_fields(gpointer key, gpointer val, gpointer user_data);
 gint get_station_weather_data(const gchar *station_id_with_path, GHashTable *data, gboolean get_detail_data);
-//gint parse_xml_data(const gchar *station_id, xmlNode *root_node, GHashTable *data);
-gint parse_xml_data(const gchar *station_id, htmlDocPtr doc, GHashTable *data);
-gint parse_xml_detail_data(const gchar *station_id, htmlDocPtr doc, GHashTable *data);
-//void fill_detail_data(xmlNode *root_node, GHashTable *day, gint part_of_day, GHashTable *hash_for_translate, GHashTable *hash_for_icons)
-//void  fill_day (xmlNode *root_node, GHashTable *day, gint part_of_day, GHashTable *hash_for_translate, GHashTable *hash_for_icons)
-GHashTable* hash_description_yrno_table_create(void);
-GHashTable* hash_icons_yrno_table_create(void);
-gpointer hash_yrno_table_find(GHashTable *hash, gpointer key, gboolean search_short_name);
+gint parse_xml_data(const gchar *station_id, xmlNode *root_node, GHashTable *data);
+gint parse_xml_detail_data(const gchar *station_id, xmlNode *root_node, GHashTable *data);
 /*******************************************************************************/
 #endif

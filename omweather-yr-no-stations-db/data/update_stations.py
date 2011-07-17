335630df6548f9c2391b1bea3cb3b8d8d09ec5e0
Vlad Vasilyeu <vasvlad@gmail.com>
2011-07-17 20:41:23 +0000

    next step for  parser for stations database from yr.no
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@4795 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-yr-no-stations-db/data/update_stations.py
+++ omweather-yr-no-stations-db/data/update_stations.py
@@ -10,20 +10,19 @@ import re
 import string
 
 
-baseurl = 'http://www.yr.no/place/'
+baseurl = 'http://www.yr.no'
 #connect to database
 c = db.connect(database=r"./yr.no.db")
 cu = c.cursor()
 
 cur = cu.execute("select name, id from countries")
 
-countries = []
-ids = []
 for row in cur:
+    # Search country region
     country_name = row[0]
     id = row[1]
-    country_name =country_name.encode('utf8')
-    country_name_url = baseurl + "/" + country_name.replace(" ","_")
+    country_name = country_name.encode('utf8')
+    country_name_url = baseurl + "/place/" + country_name.replace(" ","_")
     print country_name_url
     req = urllib2.Request(country_name_url, None, {'User-agent': 'Mozilla/5.0', 'Accept-Language':'ru'})
     page = urllib2.urlopen(req)
@@ -37,10 +36,38 @@ for row in cur:
     ctxt = doc.xpathNewContext()
     anchors = ctxt.xpathEval("//div[@class='yr-list-places clear clearfix']/dl/dd/a")
     for anchor in anchors:
-        href = anchor.prop("href")
-        print href 
-        print anchor.content 
+        cu1 = c.cursor()
+        cur1 = cu1.execute('insert into regions (name, country_id) values  ("%s", "%s")' % (anchor.content, id))
+        c.commit()
+        cur1 = cu1.execute('select id from regions where name="%s" and country_id="%s"' % (anchor.content, id))
+        for row1 in cur1:
+            id_region = row1[0]
+        c.commit()
+        region_name_url = baseurl + anchor.prop("href")
+        req = urllib2.Request(region_name_url, None, {'User-agent': 'Mozilla/5.0', 'Accept-Language':'ru'})
+        page = urllib2.urlopen(req)
 
+        fileToSave = page.read()
+        oFile = open(r"./%s.html"%(anchor.content),'wb')
+        oFile.write(fileToSave)
+        oFile.close
+        #parse xml file
+        doc = libxml2.htmlReadFile(r"./%s.html" % (anchor.content), "UTF-8", libxml2.HTML_PARSE_RECOVER)
+        ctxt = doc.xpathNewContext()
+        anchors1 = ctxt.xpathEval("//table[@class='yr-table yr-table-places yr-popup-area']/tbody/tr/th/a")
+        for anchor1 in anchors1:
+            print anchor1.content 
+            print anchor1.prop("href")
+            code = anchor1.prop("href")
+            code = code.replace("/place/", "")
+            print code
+            cu2 = c.cursor()
+            cur2 = cu2.execute('insert into stations (name, region_id, code) values  ("%s", "%s", "%s")' % (anchor1.content, id_region, code))
+            c.commit()
+
+        #print region_name_url 
+        #print anchor.content 
+        #print id_region
 
 c.commit()
 #    #Search  bad stations

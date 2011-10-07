78d57afd8a6d019336a77210d2f377458a54b6ce
Vlad Vasilyeu <vasvlad@gmail.com>
2011-10-07 12:03:33 +0000

    began update_by_country.py
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5276 5aa3d57c-301e-0410-a7f2-8fec96c11689

---
+++ omweather-yr-no-stations-db/data/update_by_country.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+#from urllib import urlopen
+import urllib2
+import urllib
+import sqlite3 as db
+import libxml2, sys
+import os
+import re
+import string
+import zipfile
+
+#Country name and code
+country = "Belarus"
+country_code = "BY"
+
+
+
+baseurl = "http://download.geonames.org/export/dump/"
+
+#connect to database
+c = db.connect(database=r"./yr.no.db")
+cu = c.cursor()
+
+#cheking country name
+cur = cu.execute("select name, id from countries where name='%s'" %(country))
+if (cur.fetchone() == None ):
+   print "Country " + country + " is absent in the database yr.no.db"
+   exit (-1);
+
+#zip filename
+myzipfile = country_code + ".zip"
+#downloading the dump file
+url = baseurl + myzipfile
+urllib.urlretrieve (url, myzipfile)
+
+#unzip file
+fh = open(myzipfile, 'rb')
+z = zipfile.ZipFile(fh)
+outfile = open(country_code + ".txt", 'wb')
+outfile.write(z.read(country_code + ".txt"))
+outfile.close()
+fh.close()
+
+fh = open(country_code + ".txt")
+for line in fh.readlines():
+    print line

50ad7aaa0338bb9897a2e2832f9aea3a1ec1747a
Vlad Vasilyeu <vasvlad@gmail.com>
2011-10-10 11:52:58 +0000

    next step for script
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5288 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-yr-no-stations-db/data/update_by_country.py
+++ omweather-yr-no-stations-db/data/update_by_country.py
@@ -82,8 +82,8 @@ for key in regions.keys():
 
 regions_name["00"] = "Other/" + country
 
+#checking regions name
 for key in regions_name.keys():
-    #checking regions name
     cur = cu.execute("select name, id from regions where country_id='%i' and name = '%s'" %(country_id,regions_name[key]))
     if (cur.fetchone() == None ):
         pattern = re.split ('(,)',regions_name_second[key])
@@ -100,12 +100,29 @@ for key in regions_name.keys():
                 print "Error in " + key + " " + regions_name[key]
     print regions_name[key]  + ' '  + key
 
+#filling stations
 fh = open(country_code + ".txt")
 for line in fh.readlines():
     pattern = re.split('(\t)', line)
     if (pattern[14] == "PPLA" or pattern[14] == "PPLC" or pattern[14] == "PPL"):
         if (pattern[20] != "" and pattern[28] != "0"):
             result = country + "#" + re.sub("Other/" + country, "Other", regions_name[pattern[20]]) + "#" + normalizing(pattern[4])
-            print result
+            cur = cu.execute("select id from regions where country_id='%i' and name = '%s'" %(country_id,regions_name[pattern[20]]))
+            region_id = None
+            for row in cur:
+                region_id = row[0]
+            cur = cu.execute("select id from stations where region_id='%i' and name = '%s'" %(region_id,normalizing(pattern[4])))
+            station_id= None
+            for row in cur:
+                station_id = row[0]
+            if (station_id == None):
+                cur = cu.execute("insert into stations (region_id, name, longititude, latitude, code) values ( '%i', '%s', '%s', '%s', '%s')" %(region_id, normalizing(pattern[4]), pattern[10], pattern[8], result))
+                c.commit()
+#                print region_id, normalizing(pattern[4]), pattern[8], pattern[10], result
+#                print "insert into stations (region_id, name, longititude, latitude, code values ( '%i', '%s', '%s', '%s', '%s')" %(region_id, normalizing(pattern[4]), pattern[8], pattern[10], result)
+#                print result
+            else:
+                cur = cu.execute("update stations set region_id ='%i', name='%s', longititude='%s', latitude='%s', code = '%s' where id ='%i'" %(region_id, normalizing(pattern[4]), pattern[10], pattern[8], result, station_id))
+                c.commit()
 fh.close
 

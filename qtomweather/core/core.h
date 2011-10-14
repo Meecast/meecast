b721d0f676f5bde298c39eec3cf9843a37b60e81
Vlad Vasilyeu <vasvlad@gmail.com>
2011-10-14 05:11:45 +0000

    began 0.3.34
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5295 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- qtomweather/core/core.h
+++ qtomweather/core/core.h
@@ -48,7 +48,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define OMWEATHER_VERSION "0.3.33"
+#define OMWEATHER_VERSION "0.3.34"
 namespace Core{
     class Connection;
     class AbstractConfig;

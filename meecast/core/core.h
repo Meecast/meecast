cfb57bef58f88b9221b25d87e1b56e07dc7dc953
Vlad Vasilyeu <vasvlad@gmail.com>
2011-10-23 10:24:52 +0000

    began version 0.4.2
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5360 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- meecast/core/core.h
+++ meecast/core/core.h
@@ -48,7 +48,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define OMWEATHER_VERSION "0.4.1"
+#define OMWEATHER_VERSION "0.4.2"
 namespace Core{
     class Connection;
     class AbstractConfig;

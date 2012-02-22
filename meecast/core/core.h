348566e3353ce49780f311b2b7b81df05127f2e8
Vlad Vasilyeu <vasvlad@gmail.com>
2012-02-22 13:41:14 +0000

    changed version to 0.5.1
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5643 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- meecast/core/core.h
+++ meecast/core/core.h
@@ -50,7 +50,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define MEECAST_VERSION "0.5"
+#define MEECAST_VERSION "0.5.1"
 namespace Core{
     class Connection;
     class AbstractConfig;

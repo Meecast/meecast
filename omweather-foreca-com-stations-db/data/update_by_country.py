aeb3178725f6a88a7a8112425da9f83ea35a33de
Vlad Vasilyeu <vasvlad@gmail.com>
2012-02-28 10:11:00 +0000

    added connect to database
    git-svn-id: https://vcs.maemo.org/svn/omweather/trunk@5677 5aa3d57c-301e-0410-a7f2-8fec96c11689

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -17,6 +17,11 @@ baseurl = "http://foreca.com/Europe/%s/browse"
 url = "http://foreca.com/%s/browse" %(country) 
 print url
 
+
+#connect to database
+c = db.connect(database=r"./foreca.com.db")
+cu = c.cursor()
+
 #urllib.urlretrieve (url, "./temp.html")
 #/html/body/div/div[2]/div[4]/div/div[2]/div[4]/p/a
 
@@ -27,15 +32,17 @@ ctxt = doc.xpathNewContext()
 anchors = ctxt.xpathEval("/html/body/div/div[2]/div[4]/div/div[2]/div[@class='col3']//a/@href")
 for anchor in anchors:
     print anchor.content
-
+    name = re.split("/", anchor.content)[-1];
     #urllib.urlretrieve ("http://foreca.com/%s/%si" %(country, anchor), "./station.html")
     doc1 = libxml2.htmlReadFile(r"./station.html", "UTF-8", libxml2.HTML_PARSE_RECOVER)
     ctxt1 = doc1.xpathNewContext()
     anchors1 = ctxt1.xpathEval("/html/body/div/div/div[4]/div/div[2]/div/div/div[2]/a")
     for anchor1 in anchors1:
         print anchor1.prop("href")
-        code = re.split("=", anchor1.prop("href"))
-        print code[1]
-    #/html/body/div/div/div[4]/div/div[2]/div/div/div[2]/a
+        code = re.split("=", anchor1.prop("href"))[1]
+        print name, "-", code;
+#        os.unlink("./station.html");
     break
 #os.unlink("./temp.html");
+#os.unlink("./temp.html");
+c.close()

bf5ad226398c980a6f6740c72ca05fdabc3b2950
Uladzislau Vasilyeu <vasvlad@gmail.com>
2012-03-12 22:11:23 +0300

    added refresh function

---
+++ meecast/tizen/src/main_window.h
@@ -0,0 +1,33 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Meecast for Tizen 
+ *
+ * Copyright (C) 2012 Vlad Vasilyeu
+ * 	for the code
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU  General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _main_h
+#define _main_h 1
+#endif
+/*******************************************************************************/
+#include <Elementary.h>
+//#include "core.h"
+#include "common.h"
+/*******************************************************************************/
+void create_main_window(void *data);
+Core::DataParser* current_data(std::string& str);

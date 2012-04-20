6e1c37a3bf449bc6be0c3e57f023d3ef6c724809
Uladzislau Vasilyeu <vasvlad@gmail.com>
2012-04-20 06:27:22 +0300

    began version 0.5.7

--- meecast/core/core.h
+++ meecast/core/core.h
@@ -50,7 +50,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define MEECAST_VERSION "0.5.6"
+#define MEECAST_VERSION "0.5.7"
 namespace Core{
     class Connection;
     class AbstractConfig;

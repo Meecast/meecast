8baf228fa00fecc87aeaae55ffd8ae63164347ce
Uladzislau Vasilyeu <vasvlad@gmail.com>
2012-04-28 19:37:46 +0300

    began omweather-hko-gov-hk-stations-db

---
+++ omweather-hko-gov-hk-stations-db/src/hash.h
@@ -0,0 +1,35 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Other Maemo Weather(omweather)
+ *
+ * Copyright (C) 2012 Vlad Vasilyeu
+ * 	for the code
+ *        
+ * Copyright (C) 2008 Andrew Zhilin
+ *		      az@pocketpcrussia.com 
+ *	for default icon set (Glance)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _hash_h
+#define _hash_h 1
+/*******************************************************************************/
+/*******************************************************************************/
+GHashTable* hash_icons_forecacom_table_create(void);
+gpointer hash_forecacom_table_find(GHashTable *hash, gpointer key, gboolean search_short_name);
+/*******************************************************************************/
+#endif

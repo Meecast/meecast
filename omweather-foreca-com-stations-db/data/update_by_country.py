014eef4b41dccd498bc04dbde8eaea941b2c2e83
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2012-05-04 16:55:53 +0300

    Added United States(Alabama, Alaska,Arizona, Arkansas, Colorado, Connecticut, Delaware,Florida,Georgia,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas) to foreca.com database

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "Europe/Portugal"
-id_region = "152" 
+country = "North America/United States/Kansas"
+id_region = "202"
 first_letter = "A"
 
 def normalizing (source):

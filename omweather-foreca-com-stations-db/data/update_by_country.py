b141eaf3d09f03aff2fcb752a752b4a6cef297bc
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2012-05-18 08:17:37 +0300

    Added Chile to foreca.com database

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,7 +13,7 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "North America/United States/Wyoming"
+country = "South America/Chile"
 id_region = "202"
 first_letter = "A"
 

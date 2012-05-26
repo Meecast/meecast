73c7eee30ddcd2a286d729d7deb6248977f48e5a
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2012-05-26 09:48:03 +0300

    Added Slovenia to foreca.com database

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "South America/Chile"
-id_region = "39"
+country = "Europe/Slovenia"
+id_region = "171"
 first_letter = "A"
 
 def normalizing (source):

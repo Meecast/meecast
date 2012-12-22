68db3c4995520105f290ba4cb9c05b9c70df0239
Uladzislau Vasilyeu <vasvlad@gmail.com>
2012-12-22 16:31:07 +0300

    added script for iconset show

--- meecast/core/data/create_icons_html.py
+++ meecast/core/data/create_icons_html.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 # vim: expandtab sw=4 ts=4 sts=4:
+import os
 
+print "<html>"
+
+dirList=os.listdir("./icons")
+#for fname in dirList:
+#    print fname
+
+print " <table>"
+text_file = open("icons_description.txt", "r")
+lines = text_file.readlines()
+for line in lines:
+    words = line.split('-')
+    print "  <tr bgcolor='#999999'>"
+    print "<td>" + words[0] + "</td>" + "<td> " + words[1] + "</td>"
+    for fname in dirList:
+        print "<td><img src='icons/" + fname + "/" +  words[0].strip() + ".png' width='128' height='128' ></td>"
+
+    print "  </tr>"
+text_file.close()
+
+
+print " </table>"
+print "</html>"

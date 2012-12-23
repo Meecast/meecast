d49ee482d8e21e5ea18d6dbe96af0f396c171ff6
Uladzislau Vasilyeu <vasvlad@gmail.com>
2012-12-23 09:53:50 +0300

    Added icons wiki page and script for it

--- meecast/core/data/create_icons_html.py
+++ meecast/core/data/create_icons_html.py
@@ -6,10 +6,15 @@ import os
 print "<html>"
 
 dirList=os.listdir("./icons")
-#for fname in dirList:
-#    print fname
+dirList.sort()
 
-print " <table>"
+print " <table width = 600>"
+print "  <tr bgcolor='#999999'>"
+print "<td>" + "N" + "</td>" + "<td> " + "Descr." + "</td>"
+for fname in dirList:
+    print "<td>" + fname + "</td>"
+
+
 text_file = open("icons_description.txt", "r")
 lines = text_file.readlines()
 for line in lines:
@@ -17,7 +22,7 @@ for line in lines:
     print "  <tr bgcolor='#999999'>"
     print "<td>" + words[0] + "</td>" + "<td> " + words[1] + "</td>"
     for fname in dirList:
-        print "<td><img src='icons/" + fname + "/" +  words[0].strip() + ".png' width='128' height='128' ></td>"
+        print "<td><img src='https://raw.github.com/Meecast/meecast/master/meecast/core/data/icons/" + fname + "/" +  words[0].strip() + ".png' width='64' height='64' ></td>"
 
     print "  </tr>"
 text_file.close()

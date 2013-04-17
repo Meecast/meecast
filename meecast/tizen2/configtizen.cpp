7a34c3c0dde321d02bdc9c2d76df882c24619fb3
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-04-17 10:52:37 +0300

    next step for adapted for tizen 2.0 - it  works

---
+++ meecast/tizen2/configtizen.cpp
@@ -0,0 +1,83 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Meecast for Tizen
+ *
+ * Copyright (C) 2012 - 2013 Vlad Vasilyeu
+ * 	for the code
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU  General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+/*******************************************************************************/
+#include "configtizen.h"
+
+ConfigTizen* ConfigTizen::_self;
+int ConfigTizen::_refcount;
+
+ConfigTizen::ConfigTizen(const std::string& filename, const std::string& schema_filename):Core::Config(filename, schema_filename)
+{
+}
+
+ConfigTizen::ConfigTizen():Core::Config()
+{
+}
+
+ConfigTizen::~ConfigTizen()
+{
+}
+
+
+ConfigTizen* 
+ConfigTizen::Instance()
+{
+    if (!_self)
+        _self = new ConfigTizen();
+    _refcount++;
+    return _self;
+}
+
+ConfigTizen* 
+ConfigTizen::Instance(const std::string& filename, const std::string& schema_filename)
+{
+    if (!_self)
+        _self = new ConfigTizen(filename, schema_filename);
+    _refcount++;
+    return _self;
+}
+
+void 
+ConfigTizen::set_screen_width(int width){
+    _screen_width = width;
+}
+
+int  
+ConfigTizen::get_screen_width(){
+    return _screen_width;
+}
+
+void 
+ConfigTizen::set_screen_height(int height){
+    _screen_height = height;
+}
+
+int  
+ConfigTizen::get_screen_height(){
+    return _screen_height;
+}
+
---
+++ meecast/tizen2/configtizen.h
@@ -0,0 +1,54 @@
+//* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Meecast for Tizen
+ *
+ * Copyright (C) 2012 - 2013 Vlad Vasilyeu
+ * 	for the code
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU  General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+
+
+#ifndef CONFIGTIZEN_H
+#define CONFIGTIZEN_H
+
+#include "../core/core.h"
+
+class ConfigTizen : public Core::Config
+
+{
+private:
+    int _screen_width;
+    int _screen_height;
+protected:
+    static ConfigTizen* _self;
+    static int _refcount;
+    virtual ~ConfigTizen();
+    ConfigTizen();
+    ConfigTizen(const std::string& filename, const std::string& schema_filename = "/usr/" + schemaPath + "config.xsd");
+
+public:
+    static ConfigTizen* Instance();
+    static ConfigTizen* Instance(const std::string& filename, const std::string& schema_filename = "/usr/" + schemaPath + "config.xsd");
+    void set_screen_width(int width);
+    int  get_screen_width();
+    void set_screen_height(int height);
+    int  get_screen_height();
+
+};
+
+#endif // CONFIGTIZEN_H

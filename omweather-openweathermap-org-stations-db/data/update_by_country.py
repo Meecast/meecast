d0e23e183163d3a5fabc3c140aa51ef3cc4dba0e
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-01 20:04:16 +0300

    Added Finland to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Russia"
-country_code = "RU"
+country = "Finland"
+country_code = "FI"
                 
 
 def normalizing4 (source):

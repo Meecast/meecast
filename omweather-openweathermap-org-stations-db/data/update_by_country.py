bf6ece93dce722904c49be6b3d8efcac687672d8
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 09:36:42 +0300

    Added Austria to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Switzerland"
-country_code = "CH"
+country = "Austria"
+country_code = "AT"
                 
 
 def normalizing4 (source):

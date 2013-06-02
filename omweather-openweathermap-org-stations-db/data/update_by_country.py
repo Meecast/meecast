ac1c2eacd573f48662f410a2b07ed7207eff2219
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 16:40:25 +0300

    Added Croatia to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Singapore"
-country_code = "SG"
+country = "Croatia"
+country_code = "HR"
                 
 
 def normalizing4 (source):

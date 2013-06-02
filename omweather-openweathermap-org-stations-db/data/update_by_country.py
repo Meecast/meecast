3f9f4fc3b7ec6c545a1ae62b1f2f481575983053
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 18:35:30 +0300

    Added Lithuania to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Venezuela"
-country_code = "VE"
+country = "Lithuania"
+country_code = "LT"
                 
 
 def normalizing4 (source):

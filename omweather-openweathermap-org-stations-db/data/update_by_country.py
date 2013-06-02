eb184063f7dbeea036d5b97ad68670dd1820e1a5
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 11:45:02 +0300

    Added Paraguay to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Ireland"
-country_code = "IE"
+country = "Paraguay"
+country_code = "PY"
                 
 
 def normalizing4 (source):

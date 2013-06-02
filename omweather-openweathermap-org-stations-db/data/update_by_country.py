d58ed0b95db7465a05c1307f63ee64a2a27179ea
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 11:48:14 +0300

    Added Georgia to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Armenia"
-country_code = "AM"
+country = "Georgia"
+country_code = "GE"
                 
 
 def normalizing4 (source):

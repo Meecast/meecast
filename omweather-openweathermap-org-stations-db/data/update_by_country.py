e2bc7f473496340b9d36c7b1f9d98645f7419c10
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 09:45:55 +0300

    Added Norway, Bulgaria to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Austria"
-country_code = "AT"
+country = "Norway"
+country_code = "NO"
                 
 
 def normalizing4 (source):

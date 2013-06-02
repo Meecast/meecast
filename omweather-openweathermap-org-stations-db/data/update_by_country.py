c3661cee3b0d521d1fdafd5f7c1dea094386e02b
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-02 11:42:45 +0300

    Added Ireland to database

--- omweather-openweathermap-org-stations-db/data/update_by_country.py
+++ omweather-openweathermap-org-stations-db/data/update_by_country.py
@@ -10,8 +10,8 @@ import os
 import re
 import string
 import zipfile
-country = "Philippines"
-country_code = "PH"
+country = "Ireland"
+country_code = "IE"
                 
 
 def normalizing4 (source):

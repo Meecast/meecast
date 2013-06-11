acf5184c759340a757b1bf996f44873ac6f9f43d
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-11 13:09:24 +0300

    Added files

---
+++ meecast/tizen2/meecastSourcesForm.h
@@ -0,0 +1,46 @@
+#ifndef _MEECAST_SOURCES_FORM_H_
+#define _MEECAST_SOURCES_FORM_H_
+
+#include <FBase.h>
+#include <FUi.h>
+#include <FIo.h>
+
+
+class meecastSourcesForm
+	: public Tizen::Ui::Controls::Form
+	, public Tizen::Ui::IActionEventListener
+	, public Tizen::Ui::Controls::IFormBackEventListener
+	, public Tizen::Ui::Controls::IListViewItemEventListener
+	, public Tizen::Ui::Controls::IListViewItemProvider
+ 	, public Tizen::Ui::Scenes::ISceneEventListener
+{
+public:
+	meecastSourcesForm(void);
+	virtual ~meecastSourcesForm(void);
+	bool Initialize(void);
+public:
+	virtual result OnInitializing(void);
+	virtual result OnTerminating(void);
+	virtual void OnActionPerformed(const Tizen::Ui::Control& source, int actionId);
+	virtual void OnFormBackRequested(Tizen::Ui::Controls::Form& source);
+	virtual void OnSceneActivatedN(const Tizen::Ui::Scenes::SceneId& previousSceneId,
+								   const Tizen::Ui::Scenes::SceneId& currentSceneId, Tizen::Base::Collection::IList* pArgs);
+	virtual void OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& currentSceneId,
+									const Tizen::Ui::Scenes::SceneId& nextSceneId);
+
+	virtual void OnListViewItemStateChanged(Tizen::Ui::Controls::ListView& listView, int index, int elementId, Tizen::Ui::Controls::ListItemStatus status);
+	virtual void OnListViewItemSwept(Tizen::Ui::Controls::ListView& listView, int index, Tizen::Ui::Controls::SweepDirection direction);
+	virtual void OnListViewContextItemStateChanged(Tizen::Ui::Controls::ListView& listView, int index, int elementId, Tizen::Ui::Controls::ListContextItemStatus state);
+	virtual void OnItemReordered(Tizen::Ui::Controls::ListView& view, int oldIndex, int newIndex);
+	virtual Tizen::Ui::Controls::ListItemBase* CreateItem(int index, int itemWidth);
+	virtual bool DeleteItem(int index, Tizen::Ui::Controls::ListItemBase* pItem, int itemWidth);
+	virtual int GetItemCount(void);
+    result GetFilesList(void);
+
+protected:
+private:
+    Tizen::Ui::Controls::ListView* __pListView;
+    Tizen::Base::Collection::ArrayList __fileList;
+};
+
+#endif	//_MEECAST_SOURCES_FORM_H_

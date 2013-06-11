b49b222a44740dddcc2b08ff83b96849971d781f
Vlad Vasilyeu <vasvlad@gmail.com>
2013-06-11 11:34:30 +0300

    fixed small problem

--- prebuild.sh
+++ prebuild.sh
@@ -7,7 +7,7 @@ cp ../omweather-openweathermap-org-stations-db/data/openweathermap.org.xml ../re
 cp ../omweather-gismeteo-ru-stations-db/data/gismeteo.ru.db ../res/db
 cp ../omweather-gismeteo-ru-stations-db/data/gismeteo.ru.xml ../res/sources
 
-cp ../src/core/data/source.xsd ../res/data/
+cp ../meecast/core/data/source.xsd ../res/data/
 rm -rf ../src/core
 cp -r ../meecast/core ../src/core 
 rm -rf ../src/tizen2

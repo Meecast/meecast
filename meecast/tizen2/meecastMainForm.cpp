62c58a68523a5dabf45369464f448a8a046590c1
Vlad Vasilyeu <vasvlad@gmail.com>
2013-06-21 16:00:08 +0300

    Added code for main temperature

--- meecast/tizen2/meecastMainForm.cpp
+++ meecast/tizen2/meecastMainForm.cpp
@@ -193,11 +193,13 @@ void
 meecastMainForm::ReInitElements(void){
 
     AppLog("REINIT ELEMENTS");
+    char  buffer[4096];
     Tizen::Ui::Controls::Label  *station_label = static_cast<Label*>(GetControl(L"IDC_LABEL_STATION_NAME"));
     Tizen::Ui::Controls::Label  *left_label = static_cast<Label*>(GetControl(L"IDC_LABEL_LEFT_BUTTON"));
     Tizen::Ui::Controls::Label  *right_label = static_cast<Label*>(GetControl(L"IDC_LABEL_RIGHT_BUTTON"));
     Tizen::Ui::Controls::Label  *main_icon = static_cast<Label*>(GetControl(L"IDC_LABEL_MAIN_ICON"));
     Tizen::Ui::Controls::Label  *main_description = static_cast<Label*>(GetControl(L"IDC_LABEL_MAIN_DESCRIPTION"));
+    Tizen::Ui::Controls::Label  *main_temperature = static_cast<Label*>(GetControl(L"IDC_LABEL_MAIN_TEMPERATURE"));
     station_label->SetText(_config->stationname().c_str());
     station_label->RequestRedraw();
     if (_config->nextstationname().length() < 1)
@@ -257,11 +259,38 @@ meecastMainForm::ReInitElements(void){
             SAFE_DELETE(image);
             SAFE_DELETE(mainIconBitmap);
         }
+
         /* Description for current weather forecast */
         String str;
         Tizen::Base::Utility::StringUtil::Utf8ToString(temp_data->Text().c_str(), str);
         main_description->SetText(str);
         main_description->RequestRedraw();
+
+        /* Temperature */
+        if (temp_data->temperature().value(true) == INT_MAX){
+          if ((temp_data->temperature_hi().value(true) == INT_MAX) &&
+              (temp_data->temperature_low().value(true) == INT_MAX)){ 
+            main_description->SetText("N/A");
+          } 
+          if ((temp_data->temperature_hi().value(true) != INT_MAX) &&
+              (temp_data->temperature_low().value(true) != INT_MAX)){ 
+            snprintf(buffer, sizeof(buffer) - 1, "%0.f°/ %0.f°", temp_data->temperature_low().value(),
+                                                                 temp_data->temperature_hi().value());
+          }  
+          if (temp_data->temperature_hi().value(true) != INT_MAX){
+            snprintf(buffer, sizeof(buffer) - 1, "%0.f°", temp_data->temperature_hi().value());
+          }
+          if (temp_data->temperature_low().value(true) != INT_MAX){
+            snprintf(buffer, sizeof(buffer) - 1, "%0.f°", temp_data->temperature_low().value());
+          }
+        }else{
+            snprintf(buffer, sizeof(buffer) - 1, "%0.f°", temp_data->temperature().value());
+        }
+
+        Tizen::Base::Utility::StringUtil::Utf8ToString(buffer, str);
+        main_temperature->SetText(str);
+        main_temperature->RequestRedraw();
+
 #if 0 
         if (!edje_object_file_set(edje_obj, "/opt/apps/com.meecast.omweather/share/edje/mainwindow.edj", "mainwindow")){
             Edje_Load_Error err = edje_object_load_error_get(edje_obj);

2bfc541eb752837f044fa515d47bd4c1dcdbd495
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-24 06:16:30 +0300

    Bgan version 0.7.4

--- meecast/core/core.h
+++ meecast/core/core.h
@@ -51,7 +51,7 @@
 #include "databaseabstract.h"
 #include "downloader.h"
 
-#define MEECAST_VERSION "0.7.3"
+#define MEECAST_VERSION "0.7.4"
 namespace Core{
     class Connection;
     class AbstractConfig;

363a171d95b29ca8e15fe5478efb7d5034967552
Uladzislau Vasilyeu <vasvlad@gmail.com>
2013-06-25 06:28:29 +0300

    Changed refresh button

--- meecast/tizen2/meecastMainForm.cpp
+++ meecast/tizen2/meecastMainForm.cpp
@@ -90,7 +90,8 @@ meecastMainForm::OnInitializing(void)
 
     FooterItem updateButton;
     updateButton.Construct(ID_BUTTON_UPDATE);
-    updateButton.SetIcon(FOOTER_ITEM_STATUS_NORMAL, Application::GetInstance()->GetAppResource()->GetBitmapN("icon_refresh.png"));
+    updateButton.SetIcon(FOOTER_ITEM_STATUS_NORMAL, Application::GetInstance()->GetAppResource()->GetBitmapN("refresh_def.png"));
+    updateButton.SetIcon(FOOTER_ITEM_STATUS_PRESSED, Application::GetInstance()->GetAppResource()->GetBitmapN("refresh_def.png"));
 
     pFooter->AddItem(updateButton);
     pFooter->AddItem(fake1menuButton);

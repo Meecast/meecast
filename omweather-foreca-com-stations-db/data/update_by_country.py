a8d3cc17f94c433c74c542696d9f46da85e92457
Ludmila Lisovskaya <ls@vlanout.(none)>
2013-07-03 15:15:16 +0300

    Added US (Connecticut,Delaware)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Colorado"
-id_region = "207"
+country = "/North America/United States/Delaware"
+id_region = "209"
 first_letter = "A"
 
 def normalizing (source):

2a2b8dc4379729dc729d087db9d9773f17ed76c6
Ludmila Lisovskaya <ls@vlanout.(none)>
2013-07-03 14:43:56 +0300

    Added US (Arkasas)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Alabama"
-id_region = "202"
+country = "/North America/United States/Arkansas"
+id_region = "205"
 first_letter = "A"
 
 def normalizing (source):

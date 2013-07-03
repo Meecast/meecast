b8d1d32f89c2e30d2003c3b9a97a6723c28b30b9
Ludmila Lisovskaya <ls@vlanout.(none)>
2013-07-03 16:37:48 +0300

    Added US (Indiana)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Illinois"
-id_region = "215"
+country = "/North America/United States/Indiana"
+id_region = "216"
 first_letter = "A"
 
 def normalizing (source):

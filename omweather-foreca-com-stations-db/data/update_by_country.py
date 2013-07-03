18a7a6a182e91c657b82a9e8c195a18a995cc13c
Ludmila Lisovskaya <ls@vlanout.(none)>
2013-07-03 15:52:57 +0300

    Added US (Hawaii)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Georgia"
-id_region = "212"
+country = "/North America/United States/Hawaii"
+id_region = "213"
 first_letter = "A"
 
 def normalizing (source):

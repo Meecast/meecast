33048b38254ea7c32cf3cf5c2d3c183a9a88d476
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2013-07-04 14:05:09 +0300

    Added US(New_Hampshire)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Nevada"
-id_region = "230"
+country = "/North America/United States/New Hampshire"
+id_region = "231"
 first_letter = "A"
 
 def normalizing (source):

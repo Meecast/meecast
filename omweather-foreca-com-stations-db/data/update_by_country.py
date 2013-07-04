c53646b264b53a79076d32c9ef3da218bd7ade08
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2013-07-04 15:47:04 +0300

    Added US(New_Mexico)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/New Jersey"
-id_region = "232"
+country = "/North America/United States/New Mexico"
+id_region = "233"
 first_letter = "A"
 
 def normalizing (source):

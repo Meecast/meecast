4af944436f9ea5418d90a6c6affd41d7989bde1a
Ludmila Lisovskaya <ls@vlanout.(none)>
2013-07-04 19:33:18 +0300

    Added US(Oklahoma)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Ohio"
-id_region = "237"
+country = "/North America/United States/Oklahoma"
+id_region = "238"
 first_letter = "A"
 
 def normalizing (source):

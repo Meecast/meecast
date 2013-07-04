b12c20338283f9fca392e1c49b411b99f8863851
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2013-07-04 17:07:44 +0300

    Added US(North Dakota)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/North Carolina"
-id_region = "235"
+country = "/North America/United States/North Dakota"
+id_region = "236"
 first_letter = "A"
 
 def normalizing (source):

cc5ee4d54f30aa62544ef29a75d7b12c459f05df
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2013-07-05 09:15:26 +0300

    Added US(Utah)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Texas"
-id_region = "246"
+country = "/North America/United States/Utah"
+id_region = "247"
 first_letter = "A"
 
 def normalizing (source):

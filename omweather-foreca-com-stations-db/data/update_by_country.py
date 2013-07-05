7bc6660a0844c0de61245b9093cfbc2f924f8464
Luidmila Lisovskaya <ludmila.lisovskaya@gmail.com>
2013-07-05 10:00:37 +0300

    Added US(Washington)

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,8 +13,8 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/North America/United States/Virginia"
-id_region = "249"
+country = "/North America/United States/Washington"
+id_region = "251"
 first_letter = "A"
 
 def normalizing (source):

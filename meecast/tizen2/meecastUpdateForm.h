8fb74dfe680aa90514f682cadc8eab8b27fafc6a
Vlad Vasilyeu <vasvlad@gmail.com>
2013-11-05 09:22:04 +0300

    Added License

--- meecast/tizen2/meecastUpdateForm.h
+++ meecast/tizen2/meecastUpdateForm.h
@@ -1,5 +1,30 @@
+/* vim: set sw=4 ts=4 et: */
+/*
+ * This file is part of Meecast for Tizen2
+ *
+ * Copyright (C) 2013 Vlad Vasilyeu
+ *     for the code
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU  General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+
 #ifndef _MEECAST_UPDATE_FORM_H_
 #define _MEECAST_UPDATE_FORM_H_
+
 
 #include <FBase.h>
 #include <FUi.h>
@@ -14,44 +39,44 @@
 
 
 class meecastUpdate
-	: public Tizen::Ui::Controls::Form
-	, public Tizen::Ui::IActionEventListener
-	, public Tizen::Ui::Controls::IFormBackEventListener
+    : public Tizen::Ui::Controls::Form
+    , public Tizen::Ui::IActionEventListener
+    , public Tizen::Ui::Controls::IFormBackEventListener
     , public Tizen::Ui::Controls::IGroupedListViewItemEventListener
     , public Tizen::Ui::Controls::IGroupedListViewItemProvider
- 	, public Tizen::Ui::Scenes::ISceneEventListener
+    , public Tizen::Ui::Scenes::ISceneEventListener
 {
 public:
-	meecastUpdate(void);
-	virtual ~meecastUpdate(void);
-	bool Initialize(void);
+    meecastUpdate(void);
+    virtual ~meecastUpdate(void);
+    bool Initialize(void);
 public:
-	virtual result OnInitializing(void);
-	virtual result OnTerminating(void);
-	virtual void OnActionPerformed(const Tizen::Ui::Control& source, int actionId);
-	virtual void OnFormBackRequested(Tizen::Ui::Controls::Form& source);
-	virtual void OnSceneActivatedN(const Tizen::Ui::Scenes::SceneId& previousSceneId,
-								   const Tizen::Ui::Scenes::SceneId& currentSceneId, Tizen::Base::Collection::IList* pArgs);
-	virtual void OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& currentSceneId,
-									const Tizen::Ui::Scenes::SceneId& nextSceneId);
+    virtual result OnInitializing(void);
+    virtual result OnTerminating(void);
+    virtual void OnActionPerformed(const Tizen::Ui::Control& source, int actionId);
+    virtual void OnFormBackRequested(Tizen::Ui::Controls::Form& source);
+    virtual void OnSceneActivatedN(const Tizen::Ui::Scenes::SceneId& previousSceneId,
+                                   const Tizen::Ui::Scenes::SceneId& currentSceneId, Tizen::Base::Collection::IList* pArgs);
+    virtual void OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& currentSceneId,
+                                    const Tizen::Ui::Scenes::SceneId& nextSceneId);
 // IGroupedListViewItemEventListener
     virtual void OnGroupedListViewItemSwept(Tizen::Ui::Controls::GroupedListView& listView, int groupIndex, int itemIndex, Tizen::Ui::Controls::SweepDirection direction) {};
-	virtual void OnGroupedListViewItemStateChanged(Tizen::Ui::Controls::GroupedListView& listView, int groupIndex, int itemIndex, int elementId, Tizen::Ui::Controls::ListItemStatus state);
+    virtual void OnGroupedListViewItemStateChanged(Tizen::Ui::Controls::GroupedListView& listView, int groupIndex, int itemIndex, int elementId, Tizen::Ui::Controls::ListItemStatus state);
     virtual void OnGroupedListViewContextItemStateChanged(Tizen::Ui::Controls::GroupedListView& listView, int groupIndex, int itemIndex, int elementId, Tizen::Ui::Controls::ListContextItemStatus state) {};
 
     // IGroupedListViewItemProvider
-	virtual int GetGroupCount(void);
-	virtual int GetItemCount(int groupIndex);
-	virtual Tizen::Ui::Controls::GroupItem* CreateGroupItem(int groupIndex, int itemWidth);
-	virtual bool DeleteGroupItem(int groupIndex, Tizen::Ui::Controls::GroupItem* pItem, int itemWidth);
-	virtual Tizen::Ui::Controls::ListItemBase* CreateItem(int groupIndex, int itemIndex, int itemWidth);
-	virtual bool DeleteItem(int groupIndex, int itemIndex, Tizen::Ui::Controls::ListItemBase* pItem, int itemWidth);
+    virtual int GetGroupCount(void);
+    virtual int GetItemCount(int groupIndex);
+    virtual Tizen::Ui::Controls::GroupItem* CreateGroupItem(int groupIndex, int itemWidth);
+    virtual bool DeleteGroupItem(int groupIndex, Tizen::Ui::Controls::GroupItem* pItem, int itemWidth);
+    virtual Tizen::Ui::Controls::ListItemBase* CreateItem(int groupIndex, int itemIndex, int itemWidth);
+    virtual bool DeleteItem(int groupIndex, int itemIndex, Tizen::Ui::Controls::ListItemBase* pItem, int itemWidth);
 
     void CreateGroupedListView(void);
 protected:
 private:
-	Tizen::Ui::Controls::GroupedListView* __pList;
-	//Tizen::Ui::Controls::ListContextItem* __pItemContext;
+    Tizen::Ui::Controls::GroupedListView* __pList;
+    //Tizen::Ui::Controls::ListContextItem* __pItemContext;
 };
 
 #endif	//_MEECAST_UPDATE_FORM_H_

6eeb2c604a88b2b034e7f5ad1f4320d58ec174fe
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-02-15 19:43:19 +0300

    fixed gcc's warnings

--- omweather-foreca-com-stations-db/src/meego-main.cpp
+++ omweather-foreca-com-stations-db/src/meego-main.cpp
@@ -65,21 +65,10 @@ parse_and_write_detail_data(const char *station_id, htmlDocPtr doc, const char *
     xmlXPathObjectPtr xpathObj2 = NULL; 
     xmlNodeSetPtr nodes;
     char       *temp_char;
-    char       *temp_char2;
-    int        pressure; 
-    int        speed;
-    int        ppcp;
-
-    char       *image = NULL;
-    double      time_diff = 0;
-    time_t      loc_time;
-    time_t      utc_time;
-    int        location_timezone = 0;
     int    flag;
     struct tm   tmp_tm = {0};
     struct tm   *tm;
     time_t      t_start = 0, t_end = 0,
-                t_sunrise = 0, t_sunset = 0,
                 current_time = 0;
     FILE        *file_out;
     int index = 1;

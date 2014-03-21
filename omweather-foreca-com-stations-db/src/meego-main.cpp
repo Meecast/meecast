8273e4d7ad760bf39a743a1871db20e58a8909be
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-03-21 18:27:56 +0300

    Fixed problem with crashing application with foreca.com data

--- omweather-foreca-com-stations-db/src/meego-main.cpp
+++ omweather-foreca-com-stations-db/src/meego-main.cpp
@@ -727,7 +727,7 @@ convert_station_forecacom_data(const char *station_id_with_path, const char *res
         if(!doc)
             return -1;
         root_node = xmlDocGetRootElement(doc);
-        if(root_node->type == XML_ELEMENT_NODE &&
+        if(root_node && root_node->type == XML_ELEMENT_NODE &&
                 strstr((char*)root_node->name, "err")){
             xmlFreeDoc(doc);
             xmlCleanupParser();

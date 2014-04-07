ce83943a9d40a51ae5e7d95657ee4ba3301b64ab
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-04-07 12:51:15 +0300

    nothing sierious

--- omweather-foreca-com-stations-db/test.sh
+++ omweather-foreca-com-stations-db/test.sh
@@ -8,3 +8,4 @@ wget -O examples/forecast_detail.html "http://foreca.mobi/spot.php?l=100620127"
 # ./forecacom ./examples/forecast.html ./examples/out.xml ./examples/forecast_detail.html
 # ./forecacom ./examples/forecast_detail.html.old ./examples/out.xml ./examples/forecast_detail.html
 valgrind --tool=memcheck --log-file=1.log -v --track-origins=yes  --leak-check=full --show-reachable=yes ./forecacom ./examples/forecast.html ./examples/out.xml ./examples/forecast_detail.html  
+#valgrind --tool=memcheck --log-file=1.log -v --track-origins=yes  --leak-check=full --show-reachable=yes ./forecacom ./examples/foreca.com_100658225.orig ./examples/out.xml ./examples/foreca.com_100658225.detail.orig  

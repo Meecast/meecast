c6647ffd86fab1670c005c97190531651659377b
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-04-09 17:41:50 +0300

    next step for fixing problem onforeca

--- omweather-foreca-com-stations-db/src/meego-main.cpp
+++ omweather-foreca-com-stations-db/src/meego-main.cpp
@@ -278,7 +278,7 @@ parse_and_write_detail_data(const char *station_id, htmlDocPtr doc, const char *
     }
     if (xpathObj)
         xmlXPathFreeObject(xpathObj);
-    
+/*    
     snprintf(buffer, sizeof(buffer)-1,"/html/body/div[@id='cc']/div[@class='cctext']/p/strong[1]/text()");
     xpathObj = xmlXPathEvalExpression((const xmlChar*)buffer, xpathCtx);
     if (xpathObj && !xmlXPathNodeSetIsEmpty(xpathObj->nodesetval) &&
@@ -312,6 +312,25 @@ parse_and_write_detail_data(const char *station_id, htmlDocPtr doc, const char *
         xpathObj->nodesetval->nodeTab[4] && xpathObj->nodesetval->nodeTab[4]->content){
         fprintf(file_out,"     <visible>%i</visible>\n", atoi((char*)xpathObj->nodesetval->nodeTab[4]->content)*1000); 
     }
+    if (xpathObj)
+        xmlXPathFreeObject(xpathObj);
+*/
+
+    snprintf(buffer, sizeof(buffer)-1,"/html/body/div[@id='cc']/div[@class='cctext']/p[2]/text()[normalize-space(.)]");
+    xpathObj = xmlXPathEvalExpression((const xmlChar*)buffer, xpathCtx);
+    if (xpathObj && !xmlXPathNodeSetIsEmpty(xpathObj->nodesetval)){
+        xmlNodeSetPtr nodeset = xpathObj->nodesetval;
+        for (int i=0; i < nodeset->nodeNr; i++) {
+	    
+            snprintf(buffer, sizeof(buffer)-1,"normalize-space(/html/body/div[@id='cc']/div[@class='cctext']/p[2]/text()[%i])", i+1);
+            xpathObj2 = xmlXPathEvalExpression((const xmlChar*)buffer, xpathCtx);
+            if (xpathObj2){
+                fprintf(stderr, "%i %s\n", i, (char*)xpathObj2->stringval);
+	    }
+	    if (xpathObj2)
+                xmlXPathFreeObject(xpathObj2);
+	}
+    } 
     if (xpathObj)
         xmlXPathFreeObject(xpathObj);
 

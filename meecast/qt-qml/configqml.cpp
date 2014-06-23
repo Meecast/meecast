6b789bab10a2fd78fcdc9f6fa9f21267a0b9ca68
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-06-23 22:36:04 +0300

    Fixed memory leaks

--- meecast/qt-qml/configqml.cpp
+++ meecast/qt-qml/configqml.cpp
@@ -518,8 +518,6 @@ ConfigQml::setgps(bool c)
             /* delete gps station */
             removeStation(index);
         }
-        if (dp)
-            dp->DeleteInstance();
     }
     ConfigQml::Config::Gps(c);
     saveConfig();

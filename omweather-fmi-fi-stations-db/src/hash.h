2221004e7b7439759088615838ebb631745cbcaa
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-07-15 12:30:23 +0300

    Merged

---
+++ omweather-fmi-fi-stations-db/src/hash.h
@@ -0,0 +1,31 @@
+/* vim: set sw=4 ts=4 et: */
+/* This file is part of omweather-fmi-fi-source - MeeCast
+ *
+ * Copyright (C) 2014 Vlad Vasilyeu
+ * 	for the code
+ *        
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+*/
+/*******************************************************************************/
+#ifndef _hash_h
+#define _hash_h 1
+/*******************************************************************************/
+#include <libxml/hash.h> 
+
+#include <string.h>
+xmlHashTablePtr hash_icons_fmifi_table_create(); 
+#endif

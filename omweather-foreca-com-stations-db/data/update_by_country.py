22253627823c18f8979ebb3a93f796165467ae46
Uladzislau Vasilyeu <vasvlad@gmail.com>
2014-08-17 08:18:23 +0300

    Added stations Lignano Pineta, Lignano Riviera, Lignano Sabbiadoro (Italy) to foreca.com source

--- omweather-foreca-com-stations-db/data/update_by_country.py
+++ omweather-foreca-com-stations-db/data/update_by_country.py
@@ -13,9 +13,9 @@ import string
 #SELECT regions.name  FROM regions WHERE regions.id NOT IN (SELECT stations.region_id FROM stations)
 
 #Country name and code
-country = "/Europe/United_Kingdom"
-id_region = "200"
-first_letter = "B"
+country = "/Europe/Italy"
+id_region = "97"
+first_letter = "L"
 
 def normalizing (source):
     result = source.replace("'","")

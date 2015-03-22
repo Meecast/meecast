c73c82d538869662e3fdc6ffe9330e518165c84a
Vlad Vasilyeu <vasvlad@gmail.com>
2015-03-22 19:03:32 +0300

    Updated changelog

--- omweather-fmi-fi-stations-db/src/meego-main.cpp
+++ omweather-fmi-fi-stations-db/src/meego-main.cpp
@@ -2,7 +2,7 @@
 /*
  * This file is part of omweather-fmi-fi-source - MeeCast
  *
- * Copyright (C) 2014 Vlad Vasilyeu
+ * Copyright (C) 2014-2015 Vlad Vasilyeu
  * 	for the code
  *
  * This software is free software; you can redistribute it and/or

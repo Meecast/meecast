159f8381a818d4fd3c77cf900e9cd91a6c849f1a
Uladzislau Vasilyeu <vasvlad@gmail.com>
2015-09-20 19:05:43 +0300

    began version 1.1.6

--- meecast/qt-qml/dataitem.h
+++ meecast/qt-qml/dataitem.h
@@ -2,7 +2,7 @@
 /*
  * This file is part of Other Maemo Weather(omweather) - MeeCast
  *
- * Copyright (C) 2006-2014 Vlad Vasilyeu
+ * Copyright (C) 2006-2015 Vlad Vasilyeu
  * Copyright (C) 2010-2011 Tanya Makova
  *     for the code
  *

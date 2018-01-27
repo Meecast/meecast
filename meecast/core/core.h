b35c275755808d2f25bfd14afdd0312aa981307c
Uladzislau Vasilyeu <vasvlad@gmail.com>
2018-01-27 19:17:48 +0300

    Began version 1.1.20

--- meecast/core/core.h
+++ meecast/core/core.h
@@ -2,7 +2,7 @@
 /*
  * This file is part of Other Maemo Weather(omweather) - MeeCast
  *
- * Copyright (C) 2006-2016 Vlad Vasilyeu
+ * Copyright (C) 2006-2018 Vlad Vasilyeu
  * Copyright (C) 2006-2011 Pavel Fialko
  * Copyright (C) 2010-2011 Tanya Makova
  *     for the code

9ed2c619ede682b965fc10fb14b778259dbb206a
Uladzislau Vasilyeu <vlad@oblgas.by>
2020-01-19 09:46:37 +0300

    began make 'today' for weatther.com

--- omweather-weather-com-stations-db/test.sh
+++ omweather-weather-com-stations-db/test.sh
@@ -4,4 +4,4 @@
 #wget -O examples/forecast_detail.xml "http://wxdata.weather.com/wxdata/weather/local/BOXX0014?cm_ven=1CW&site=wx.com-bar&cm_ite=wx-cc&par=1CWFFv1.1.9&cm_pla=wx.com-bar&cm_cat=FFv1.1.9&unit=m&hbhf=12"
 #./weathercom ./examples/forecast.xml ./examples/out.xml ./examples/forecast_detail.xml 
 #./weathercom ./examples/BUG.newYear/weather.com_BOXX0014.orig ./examples/BUG.newYear/out.xml ./examples/BUG.newYear/weather.com_BOXX0014.detail.orig 
-./weathercom ./examples/BOXX0014.orig ./examples/out.xml 
+./weathercom ./examples/BOXX0014.orig ./examples/out.xml ./examples/BOXX0014.orig.detail

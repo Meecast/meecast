7f5f0b53bbb00c19248f6a3062d9295538ebe4a1
olf <Olf0@users.noreply.github.com>
2024-06-04 03:03:02 +0200

    Update hash.h

--- omweather-meteonova-ru-stations-db/src/hash.h
+++ omweather-meteonova-ru-stations-db/src/hash.h
@@ -10,16 +10,15 @@
  *	for default icon set (Glance)
  *
  * This software is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
  *
  * This software is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU  General Public
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA

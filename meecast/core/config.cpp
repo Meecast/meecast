ddfe8fa18eb6b140f34d999ff7067ff7c0a7a6f6
Uladzislau Vasilyeu <vasvlad@gmail.com>
2024-11-02 13:17:46 +0300

    Trying to fix problem with readonly config file

--- meecast/core/config.cpp
+++ meecast/core/config.cpp
@@ -2,7 +2,7 @@
 /*
  * This file is part of Other Maemo Weather(omweather) - MeeCast
  *
- * Copyright (C) 2006-2019 Vlad Vasilyeu
+ * Copyright (C) 2006-2024 Vlad Vasilyeu
  * Copyright (C) 2006-2011 Pavel Fialko
  * Copyright (C) 2010-2011 Tanya Makova
  *     for the code

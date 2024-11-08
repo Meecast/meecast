968903fadac6f6257ac8902f005a6aea9369ede0
Uladzislau Vasilyeu <vasvlad@gmail.com>
2024-11-08 21:46:14 +0300

    added setOrganizationName

--- meecast/qt-qml/main.cpp
+++ meecast/qt-qml/main.cpp
@@ -101,6 +101,9 @@ Q_DECL_EXPORT int main(int argc, char* argv[])
      
     //QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QGuiApplication* app(Aurora::Application::application(argc, argv));
+    application->setOrganizationName(QStringLiteral("org.meecast"));
+    application->setApplicationName(QStringLiteral("MeeCast"));
+
     qmlRegisterType<MeeCastCover>("harbour.meecast.meecastcover", 1, 0, "MeeCastCover");
     //app->setProperty("NoMStyle", true);
 
